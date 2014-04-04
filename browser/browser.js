85c1c95d1763b719b611483f8ee2e87114e55ec3
xifei <xifei@nfs.iscas.ac.cn>
2014-04-04 15:10:26 +0800

    add frontend in dir browser.

---
+++ browser/browser.js
@@ -0,0 +1,71 @@
+var paths=[];
+var dirpath=document.getElementById("label-paths");
+//console.log(dirpath);
+
+function opendir()
+{
+  td=document.getElementById(this.id);
+  console.log("文件夹"+td.textContent);
+  paths.push(td.textContent);
+  var path="";
+  for(var i=0;i<paths.length;i++)
+    path+=paths[i]+"/";
+  dirpath.textContent=path;
+}
+function appendtr(table, content)
+{
+  var id=table.children.length;
+  tr = document.createElement("tr");
+  tr.id = "tr"+id;
+  for(var i=0;i<4;i++){
+    if(content[i]==undefined)
+      continue;
+    td = document.createElement("td");
+    td.id=4*id+i;
+    td.className="out";
+    td.width="100";
+    td.height="100";
+    td.onclick=opendir;
+    td.onmouseover=function(){this.className="over";};
+    td.onmouseout=function(){this.className="out";};
+    td.textContent=content[i];
+    tr.appendChild(td);
+  }
+  table.appendChild(tr);
+}
+
+function loadfiles(){
+  var table=document.getElementById("table-files");
+  var content=["音乐", "视频", "图像","游戏", "联系人"];
+  for(var i=0;i<content.length/4+1;i++){
+    appendtr(table, content.slice(i*4, (i+1)*4));
+  }
+//for(var i=0;i<table.children.length;i++){
+//  console.log(table.children[i]);}
+}
+window.onload=loadfiles;
+
+//  log = document.createElement("tr");
+//  log.id = "tr1";
+//  log.innerHTML = "<td id='0' bgcolor='green' width='100' height='100' onclick='getid(this.id)'>文件夹0</td>";
+//  table.appendChild(log);
+
+
+//function loadfiles(){
+//  document.write("<hr color=\"red\" />");                         // Begin an HTML table
+//  document.write("<table border=\"1\" align='center'>");                         // Begin an HTML table
+//  document.write("<caption>File Browser</caption>");  // Output table header
+//  var files=[]
+//  for(var i=0; i<16; i++){
+//      files.push("文件夹"+i)
+//  }
+//  for(var i=0; i<4; i++){
+//      document.write("<tr>");
+//      for(var j=0; j<4; j++) {                     // Output 10 rows
+//        document.write("<td id=" + (i*4+j)  + " onclick=\"getid(this.id)\">" + files[i*4+j]  + "</td>");
+//      }
+//      document.write("</tr>");
+//  }
+//  document.write("</table>");
+//}
+
