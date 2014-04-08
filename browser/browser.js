62f1fb76e9a5a4ffde830d8d185562bf76b1ac2d
xifei <xifei@nfs.iscas.ac.cn>
2014-04-08 11:00:44 +0800

    modify files in browser.

--- browser/browser.js
+++ browser/browser.js
@@ -2,44 +2,94 @@ var paths=[];
 var dirpath=document.getElementById("label-paths");
 //console.log(dirpath);
 
+function getTextSync(url)
+{
+    var request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send(null);
+    if(request.status !== 200)
+    {
+        throw new Error(request.statusText);
+    }
+    var type=request.getResponseHeader("Content-Type");
+    if(!type.match(/^text/))
+    {
+        throw new Error("Expected textual response; got: " + type);
+    }
+    return request.responseText;
+}
+function callback(text)
+{
+    console.log(text);
+}
+function getText(url, callback)
+{
+    var request=new XMLHttpRequest();
+    request.open("GET", url);
+    request.onreadystatechange = function(){
+        if(request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader("Content-Type");
+            console.log("type:"+type);
+            if(type.match(/^text/))
+                callback(request.responseText);
+         }
+    };
+    request.send(null);
+}
+function postText(url, callback)
+{
+    var request=new XMLHttpRequest();
+    request.open("POST", url);
+    request.onreadystatechange = function(){
+        if(request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader("Content-Type");
+            if(type.match(/^text/))
+                callback(request.responseText);
+         }
+    };
+    request.setRequestHeader("Content-Type", "application/json")
+    request.send(null);
+}
+
 function opendir()
 {
-  td=document.getElementById(this.id);
-  console.log("文件夹"+td.textContent);
-  paths.push(td.textContent);
-  var path="";
-  for(var i=0;i<paths.length;i++)
-    path+=paths[i]+"/";
-  dirpath.textContent=path;
+    td=document.getElementById(this.id);
+    console.log("文件夹"+td.textContent);
+    paths.push(td.textContent);
+    var path="";
+    for(var i=0;i<paths.length;i++)
+        path+=paths[i]+"/";
+    dirpath.textContent=path;
+    console.log(getText("js/browser.html", callback));
 }
 function appendtr(table, content)
 {
-  var id=table.children.length;
-  tr = document.createElement("tr");
-  tr.id = "tr"+id;
-  for(var i=0;i<4;i++){
-    if(content[i]==undefined)
-      continue;
-    td = document.createElement("td");
-    td.id=4*id+i;
-    td.className="out";
-    td.width="100";
-    td.height="100";
-    td.onclick=opendir;
-    td.onmouseover=function(){this.className="over";};
-    td.onmouseout=function(){this.className="out";};
-    td.textContent=content[i];
-    tr.appendChild(td);
-  }
-  table.appendChild(tr);
+    var id=table.children.length;
+    tr = document.createElement("tr");
+    tr.id = "tr"+id;
+    for(var i=0;i<4;i++){
+        if(content[i]==undefined)
+            continue;
+        td = document.createElement("td");
+        td.id=4*id+i;
+        td.className="out";
+        td.width="100";
+        td.height="100";
+        td.onclick=opendir;
+        td.onmouseover=function(){this.className="over";};
+        td.onmouseout=function(){this.className="out";};
+        td.textContent=content[i];
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
 }
 
 function loadfiles(){
-  var table=document.getElementById("table-files");
-  var content=["音乐", "视频", "图像","游戏", "联系人"];
-  for(var i=0;i<content.length/4+1;i++){
-    appendtr(table, content.slice(i*4, (i+1)*4));
-  }
+    var table=document.getElementById("table-files");
+    var content=["音乐", "视频", "图像","游戏", "联系人"];
+    for(var i=0;i<content.length/4+1;i++){
+        appendtr(table, content.slice(i*4, (i+1)*4));
+    }
 //for(var i=0;i<table.children.length;i++){
 //  console.log(table.children[i]);}
 }
