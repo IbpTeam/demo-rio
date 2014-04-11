907d4b48b82d2747bd054542f83c35a43cef69ab
Hu Yuanzhe <yuanzhe@iscas.ac.cn>
2014-04-11 17:45:15 +0800

    add method in category dao

--- nodewebkit/DAO/CategoryDAO.js
+++ nodewebkit/DAO/CategoryDAO.js
@@ -1,6 +1,7 @@
 var sqlite3 = require('sqlite3');
 var events = require('events');
 var categoryEmitter = new events.EventEmitter();
+
 //连接数据库
 function openDB(){
   return new sqlite3.Database('/home/cos/demo-rio/nodewebkit/db/rio');
@@ -17,15 +18,26 @@ exports.getEmitter = function(){
   return emitter;
 }
 
-//查询category表中所有数据
+/**
+ * @method findAll
+ *   查询category表中所有数据
+ * @param null
+ *
+ * @return category
+ *   数组对象，数组中每一个元素都是一条数据对象
+ */
 exports.findAll = function(){
   var db = openDB();
   db.all("select * from Category", findAllCallBack);
-  //.log(id);
   closeDB(db);
 }  
-
+//findAll方法回调函数
 function findAllCallBack(err, rows){
+  if(err){
+    console.log(err);
+    categoryEmitter.emit('findAll', null);
+    return;
+  }
   var category = new Array();
   rows.forEach(function (row){
     category.push({
@@ -34,12 +46,55 @@ function findAllCallBack(err, rows){
       desc:row.desc
     });
   });
-  console.log('=========================');
-  console.log(category);
   categoryEmitter.emit('findAll', category);
 }
 
-//根据ID查询表中指定数据
-exports.findById = function(id, callback){
+/**
+ * @method findById
+ *   根据ID查询表中指定数据
+ * @param id
+ *   category表中的主键
+ * @return category
+ *   数组对象，数组中仅有一条指定返回的数据对象
+ */
+exports.findById = function(id){
   var db = openDB();
+  db.get("select * from Category where id = ?", id, findByIdCallBack);
+  closeDB();
+}
+//findById方法回调函数
+function findByIdCallBack(err, row){
+  if(err){
+    console.log(err);
+    categoryEmitter.emit('findById', null);
+  }
+  var category = new Array();
+  category.push({
+    id:row.id,
+    type:row.type,
+    desc:row.desc
+  });
+  categoryEmitter.emit('findById', category);
+}
+/**
+ * @method countTotal
+ *   查询Category表中类别总数
+ * @param null
+ *   
+ * @return total
+ *   Integer 表中类别总数
+ */
+exports.countTotal = function(){
+  var db = openDB();
+  db.get("select count(*) as total from Category", countTotalCallBack);
+  closeDB();  
+}
+//countTotal方法回调函数
+function countTotalCallBack(err, row){
+  if(err){
+    console.log(err);
+    categoryEmitter.emit('countTotal', null);
+  }
+  var total = row.total;
+  category.Emitter.emit('countTotal', total);
 }
