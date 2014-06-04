99969db67cef79334fc10d8ba69bab55b63602a7
“wangfeng” <wangfeng@nfs.iscas.ac.cn>
2014-06-04 17:29:35 +0800

    modify resource folder

--- nodewebkit/backend/config.js
+++ nodewebkit/backend/config.js
@@ -1,4 +1,4 @@
 var PROJECTHOME="/home/h/demo-rio/nodewebkit/backend";
-var RESOURCEPATH='/home/v1/demo-resources';
+var RESOURCEPATH='/home/v1/demo-rio/nodewebkit/resources';
 exports.projecthome = PROJECTHOME;
 exports.RESOURCEPATH = RESOURCEPATH;
