ebb8f565ad250ead9444e4ebfdd2f274f061a5c4
xifei <xifei@nfs.iscas.ac.cn>
2014-06-23 21:36:32 +0800

    ready to try code for both local and browser.

--- nodewebkit/frontend-dev/main.js
+++ nodewebkit/frontend-dev/main.js
@@ -1,41 +1,45 @@
 global.$ = $;
-
-//console.log(global.__dirname);
-//console.log(global.__filename);
-//console.log("path:" + process.cwd());
-//var sbar = require("side_bar.js");//./file-explorer/node_modules/
-//var abar = require("address_bar.js");//./file-explorer/node_modules/
-//var folder_view = require('folder_view.js');//./file-explorer/node_modules/
+global.getAllCate = getAllCate;
+global.getAllDataByCate = getAllDataByCate;
+global.rmDataById = rmDataById;
+global.getDataById = getDataById;
+global.getDataSourceById = getDataSourceById;
+global.updateDataValue = updateDataValue;
+global.getRecentAccessData = getRecentAccessData;
+        
+var sbar = require("./frontend-dev/node_modules/side_bar.js");//
+var abar = require("./frontend-dev/node_modules/address_bar.js");//
+var folder_view = require('./frontend-dev/node_modules/folder_view.js');//
 //var path = require('path');
 //var shell = require('nw.gui').Shell;
-
-//    function get_data(text){
-//	console.log('data from server:', text);
-//    }
 
 $(document).ready(function() {
     //getAllCate(get_data);
     configuration();
-    var sidebar = new SideBar($('#sidebar'));
-	var folder = new Folder($('#files'));
-	var addressbar = new AddressBar($('#addressbar'));
+    var sidebar = new sbar.SideBar($('#sidebar'));
+	var addressbar = new abar.AddressBar($('#addressbar'));
+	var folder = new folder_view.Folder($('#files'));
+
+//  var sidebar = new SideBar($('#sidebar'));
+//	var folder = new Folder($('#files'));
+//	var addressbar = new AddressBar($('#addressbar'));
 
 	folder.open('root');//process.cwd()
-	addressbar.set('root');//
+	addressbar.set('root');
 
 	folder.on('navigate', function(mime) {
-		if (mime['props'].type == 'folder') {
+//		if (mime['props'].type == 'folder') {
 		    sidebar.set_favorites_focus(mime);
 			addressbar.enter(mime);
-		}
-		else {
-			//shell.openItem(mime.path);
-			var file_propery='';
-		    for(var key in mime){
-			    file_propery += key + ':' + mime[key] + '\n';
-		    }
-		    alert(file_propery);
-		}
+//		}
+//		else {
+//			//shell.openItem(mime.path);
+//			var file_propery='';
+//		    for(var key in mime){
+//			    file_propery += key + ':' + mime[key] + '\n';
+//		    }
+//		    alert(file_propery);
+//		}
 	});
 	folder.on('folder_set_favorites', function(dirs) {
 	    sidebar.set_favorites(dirs);
