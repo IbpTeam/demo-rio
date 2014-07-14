5459fae1b251d23d645d8cf94746d8f5f8e5f887
Hu Yuanzhe <yuanzhe@iscas.ac.cn>
2014-07-14 09:55:31 +0800

    add mdns necessary file

---
+++ nodewebkit/node_modules/mdns/build/Release/.deps/Release/obj.target/dns_sd_bindings/src/dns_service_ref_sock_fd.o.d
@@ -0,0 +1,29 @@
+cmd_Release/obj.target/dns_sd_bindings/src/dns_service_ref_sock_fd.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/h/.nw-gyp/0.8.4/src -I/home/h/.nw-gyp/0.8.4/deps/uv/include -I/home/h/.nw-gyp/0.8.4/deps/v8/include  -Wall -Wextra -Wno-unused-parameter -pthread -m32 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/dns_sd_bindings/src/dns_service_ref_sock_fd.o.d.raw  -c -o Release/obj.target/dns_sd_bindings/src/dns_service_ref_sock_fd.o ../src/dns_service_ref_sock_fd.cpp
+Release/obj.target/dns_sd_bindings/src/dns_service_ref_sock_fd.o: \
+ ../src/dns_service_ref_sock_fd.cpp ../src/mdns.hpp \
+ ../src/mdns_settings.hpp /home/h/.nw-gyp/0.8.4/src/node.h \
+ /home/h/.nw-gyp/0.8.4/deps/uv/include/uv.h \
+ /home/h/.nw-gyp/0.8.4/deps/uv/include/uv-private/uv-unix.h \
+ /home/h/.nw-gyp/0.8.4/deps/uv/include/uv-private/ngx-queue.h \
+ /home/h/.nw-gyp/0.8.4/deps/uv/include/uv-private/uv-linux.h \
+ /home/h/.nw-gyp/0.8.4/deps/v8/include/v8.h \
+ /home/h/.nw-gyp/0.8.4/deps/v8/include/v8stdint.h \
+ /home/h/.nw-gyp/0.8.4/src/node_object_wrap.h \
+ /home/h/.nw-gyp/0.8.4/src/node.h \
+ /home/h/.nw-gyp/0.8.4/src/node_version.h ../src/mdns_utils.hpp \
+ ../src/dns_service_ref.hpp
+../src/dns_service_ref_sock_fd.cpp:
+../src/mdns.hpp:
+../src/mdns_settings.hpp:
+/home/h/.nw-gyp/0.8.4/src/node.h:
+/home/h/.nw-gyp/0.8.4/deps/uv/include/uv.h:
+/home/h/.nw-gyp/0.8.4/deps/uv/include/uv-private/uv-unix.h:
+/home/h/.nw-gyp/0.8.4/deps/uv/include/uv-private/ngx-queue.h:
+/home/h/.nw-gyp/0.8.4/deps/uv/include/uv-private/uv-linux.h:
+/home/h/.nw-gyp/0.8.4/deps/v8/include/v8.h:
+/home/h/.nw-gyp/0.8.4/deps/v8/include/v8stdint.h:
+/home/h/.nw-gyp/0.8.4/src/node_object_wrap.h:
+/home/h/.nw-gyp/0.8.4/src/node.h:
+/home/h/.nw-gyp/0.8.4/src/node_version.h:
+../src/mdns_utils.hpp:
+../src/dns_service_ref.hpp:
