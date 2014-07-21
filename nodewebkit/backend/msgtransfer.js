ebef18ac4e7c2e1ac1e70e1704a1e49811da2e50
rtty122333 <wangbo7799@gmail.com>
2014-07-21 09:56:22 +0800

    deal with error

--- nodewebkit/backend/msgtransfer.js
+++ nodewebkit/backend/msgtransfer.js
@@ -1,6 +1,7 @@
 var net = require('net')
 var config = require('./config')
 
+
 function initServer(){
 	var server =  net.createServer(function(c) {
 		console.log('Client ' + c.remoteAddress + ' : ' + c.remotePort + ' connected!');
@@ -17,15 +18,23 @@ function initServer(){
 			console.log('Client ' + remoteAD +  ' : ' + remotePT + ' disconnected!');
 		});
 	c.write('Hello!\r\n');
+
+	c.on('error',function(){
+			console.log('Unexpected Error!');
+	});
 //	c.pipe(c);
 	});
+
+
 
 	server.listen(config.MSGPORT, function(){
 		console.log('Server Binded! '+ config.MSGPORT);
 	});
 }
 
-function sendMSG(IP,MSG){
+
+
+function sendMsg(IP,MSG){
 	if ( !net.isIP(IP)) {
 		console.log('Input IP Format Error!');
 		return;
@@ -39,4 +48,4 @@ function sendMSG(IP,MSG){
 }
 
 exports.initServer=initServer;
-exports.sendMSG=sendMSG;
+exports.sendMsg=sendMsg;
