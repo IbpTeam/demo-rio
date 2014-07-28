1f9bca401bda6e4e033e5e3956171b73a226d003
Hu Yuanzhe <yuanzhe@iscas.ac.cn>
2014-07-28 17:13:43 +0800

    Data#40_yuanzhe:add param uri in update value methods

--- nodewebkit/backend/api.js
+++ nodewebkit/backend/api.js
@@ -180,16 +180,16 @@ function getDataSourceById(getDataSourceByIdCb,id){
 //成功返回success;
 //失败返回失败原因
 
-function updateDataValue(updateDataValueCb,id,key,value){
+function updateDataValue(updateDataValueCb,id,uri,key,value){
   console.log("Request handler 'updateDataValue' was called.");
   if(isLocal()){     
     console.log('You are in local '); 
     var apiLocalHandle = require("./backend/apiLocalHandle");
-    apiLocalHandle.updateDataValueFromLocal(updateDataValueCb,id,key,value);
+    apiLocalHandle.updateDataValueFromLocal(updateDataValueCb,id,uri,key,value);
   }
   else{
     console.log('You are in remote '); 
-    updateDataValueFromHttp(updateDataValueCb,id,key,value);
+    updateDataValueFromHttp(updateDataValueCb,id,uri,key,value);
   }
 }
 
