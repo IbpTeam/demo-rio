99e47aee4f71549df6ae3c713a9a3d3acbda6684
“wangfeng” <wangfeng@nfs.iscas.ac.cn>
2014-07-30 10:32:14 +0800

    Add nodegit module

---
+++ nodewebkit/node_modules/nodegit/build/Release/.deps/Release/obj.target/libgit2/vendor/libgit2/src/date.o.d
@@ -0,0 +1,37 @@
+cmd_Release/obj.target/libgit2/vendor/libgit2/src/date.o := cc '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DGIT_THREADS' '-DSRC_UTIL_H_' -I/home/v1/.node-gyp/0.10.29/src -I/home/v1/.node-gyp/0.10.29/deps/uv/include -I/home/v1/.node-gyp/0.10.29/deps/v8/include -I../vendor/libgit2/include -I../vendor/libgit2/src -I../vendor/libgit2/deps/regex -I../vendor/libgit2/deps/zlib -I../vendor/libgit2/deps/http-parser  -Wall -Wextra -Wno-unused-parameter -pthread -m32 -w -Wno-missing-field-initializers -Wno-unused-variable -Wno-deprecated-declarations -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/libgit2/vendor/libgit2/src/date.o.d.raw  -c -o Release/obj.target/libgit2/vendor/libgit2/src/date.o ../vendor/libgit2/src/date.c
+Release/obj.target/libgit2/vendor/libgit2/src/date.o: \
+ ../vendor/libgit2/src/date.c ../vendor/libgit2/src/common.h \
+ ../vendor/libgit2/include/git2/common.h \
+ ../vendor/libgit2/src/cc-compat.h ../vendor/libgit2/include/git2/types.h \
+ ../vendor/libgit2/include/git2/common.h \
+ ../vendor/libgit2/include/git2/errors.h \
+ ../vendor/libgit2/src/thread-utils.h ../vendor/libgit2/src/bswap.h \
+ ../vendor/libgit2/deps/regex/regex.h ../vendor/libgit2/src/util.h \
+ ../vendor/libgit2/src/cache.h ../vendor/libgit2/include/git2/oid.h \
+ ../vendor/libgit2/include/git2/types.h \
+ ../vendor/libgit2/include/git2/odb.h \
+ ../vendor/libgit2/include/git2/oid.h \
+ ../vendor/libgit2/include/git2/odb_backend.h \
+ ../vendor/libgit2/include/git2/indexer.h ../vendor/libgit2/src/posix.h \
+ ../vendor/libgit2/src/fnmatch.h ../vendor/libgit2/src/unix/posix.h
+../vendor/libgit2/src/date.c:
+../vendor/libgit2/src/common.h:
+../vendor/libgit2/include/git2/common.h:
+../vendor/libgit2/src/cc-compat.h:
+../vendor/libgit2/include/git2/types.h:
+../vendor/libgit2/include/git2/common.h:
+../vendor/libgit2/include/git2/errors.h:
+../vendor/libgit2/src/thread-utils.h:
+../vendor/libgit2/src/bswap.h:
+../vendor/libgit2/deps/regex/regex.h:
+../vendor/libgit2/src/util.h:
+../vendor/libgit2/src/cache.h:
+../vendor/libgit2/include/git2/oid.h:
+../vendor/libgit2/include/git2/types.h:
+../vendor/libgit2/include/git2/odb.h:
+../vendor/libgit2/include/git2/oid.h:
+../vendor/libgit2/include/git2/odb_backend.h:
+../vendor/libgit2/include/git2/indexer.h:
+../vendor/libgit2/src/posix.h:
+../vendor/libgit2/src/fnmatch.h:
+../vendor/libgit2/src/unix/posix.h:
---
+++ nodewebkit/node_modules/nodegit/build/Release/.deps/Release/obj.target/libgit2/vendor/libgit2/src/hash.o.d
@@ -0,0 +1,28 @@
+cmd_Release/obj.target/libgit2/vendor/libgit2/src/hash.o := cc '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DGIT_THREADS' '-DSRC_UTIL_H_' -I/home/v1/.node-gyp/0.10.29/src -I/home/v1/.node-gyp/0.10.29/deps/uv/include -I/home/v1/.node-gyp/0.10.29/deps/v8/include -I../vendor/libgit2/include -I../vendor/libgit2/src -I../vendor/libgit2/deps/regex -I../vendor/libgit2/deps/zlib -I../vendor/libgit2/deps/http-parser  -Wall -Wextra -Wno-unused-parameter -pthread -m32 -w -Wno-missing-field-initializers -Wno-unused-variable -Wno-deprecated-declarations -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/libgit2/vendor/libgit2/src/hash.o.d.raw  -c -o Release/obj.target/libgit2/vendor/libgit2/src/hash.o ../vendor/libgit2/src/hash.c
+Release/obj.target/libgit2/vendor/libgit2/src/hash.o: \
+ ../vendor/libgit2/src/hash.c ../vendor/libgit2/src/common.h \
+ ../vendor/libgit2/include/git2/common.h \
+ ../vendor/libgit2/src/cc-compat.h ../vendor/libgit2/include/git2/types.h \
+ ../vendor/libgit2/include/git2/common.h \
+ ../vendor/libgit2/include/git2/errors.h \
+ ../vendor/libgit2/src/thread-utils.h ../vendor/libgit2/src/bswap.h \
+ ../vendor/libgit2/deps/regex/regex.h ../vendor/libgit2/src/util.h \
+ ../vendor/libgit2/src/hash.h ../vendor/libgit2/include/git2/oid.h \
+ ../vendor/libgit2/include/git2/types.h \
+ ../vendor/libgit2/src/hash/hash_generic.h ../vendor/libgit2/src/hash.h
+../vendor/libgit2/src/hash.c:
+../vendor/libgit2/src/common.h:
+../vendor/libgit2/include/git2/common.h:
+../vendor/libgit2/src/cc-compat.h:
+../vendor/libgit2/include/git2/types.h:
+../vendor/libgit2/include/git2/common.h:
+../vendor/libgit2/include/git2/errors.h:
+../vendor/libgit2/src/thread-utils.h:
+../vendor/libgit2/src/bswap.h:
+../vendor/libgit2/deps/regex/regex.h:
+../vendor/libgit2/src/util.h:
+../vendor/libgit2/src/hash.h:
+../vendor/libgit2/include/git2/oid.h:
+../vendor/libgit2/include/git2/types.h:
+../vendor/libgit2/src/hash/hash_generic.h:
+../vendor/libgit2/src/hash.h:
