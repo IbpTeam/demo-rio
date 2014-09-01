69615337f59677e8bc9d393ac6e5cda9ea451aa4
“shuanzi” <daixiquan@gmail.com>
2014-09-01 17:17:40 +0800

    test with new msgtrasfer:fix bug

--- nodewebkit/backend/msg.js
+++ nodewebkit/backend/msg.js
@@ -1,6 +1,7 @@
 //msg.js
 
 var WebSocketServer = require('ws').Server;
+var WebSocket = require('ws');
 //var net = require('net');
 //var server = require('socket.io')();
 //var io = require('socket.io/node_modules/socket.io-client');
@@ -12,11 +13,14 @@ function initServer(){
 	server = new WebSocketServer({port: config.MSGPORT});
 
 	server.on('connection',function(c) {
+		//console.log("00000000000000000000"+c._socket.remoteAddress)
 		console.log('messages ' + c.remoteAddress + ' : ' + c.remotePort + ' connected!');
-		var remoteAD = c.remoteAddress;
-		var remotePT = c.remotePort;
+		var remoteAD = c._socket.remoteAddress;
+		var remotePT = c._socket.remotePort;
 
 	c.on('message', function(msgStr) {
+		console.log(msgStr)
+
 		console.log('data from :' + remoteAD+ ': ' + remotePT+ ' ' + msgStr);
 		var msgObj = JSON.parse(msgStr);
 		console.log('data from :' + remoteAD+ ': ' + remotePT+ ' ' + msgObj.type);
@@ -51,16 +55,12 @@ function initServer(){
 	c.on('close',function(){
 			console.log('Client ' + remoteAD +  ' : ' + remotePT + ' disconnected!');
 		});
-	c.write('Hello!\r\n');
+	//c.write('Hello!\r\n');
 
 	c.on('error',function(){
 			console.log('Unexpected Error!');
 	});
 //	c.pipe(c);
-	});
-
-	server.listen(config.MSGPORT, function(){
-		console.log('Server Binded! '+ config.MSGPORT);
 	});
 }
 
@@ -73,15 +73,15 @@ function sendMsg(IP,MSG){
 	//};
 //	console.log("=========================="+config.SERVERIP)
 
-    var ws = new WebSocket('http://'+IP+':'config.MSGPORT);
+    var ws = new WebSocket('http://'+IP+':'+config.MSGPORT);
 
 	if (IP == config.SERVERIP) {
 		console.log("Input IP is localhost!");
 		return;
 	};
-	
+
     ws.on('open', function() {
-    ws.send('something');
+    ws.send(MSG);
     });
 	//var  client = new _client();
 	//client.on('connect',function(){
@@ -89,10 +89,10 @@ function sendMsg(IP,MSG){
 	//	client.send(MSG);
 	//});
 
-	//client.on('error',function(err){
-	//	console.log("Error: "+err.code+" on "+err.syscall+" !  IP : " + IP);
-	//	client.end();
-	//});
+	ws.on('error',function(err){
+		console.log("Error: "+err.code+" on "+err.syscall+" !  IP : " + IP);
+		//ws.end();
+	});
 }
 
 exports.initServer=initServer;
