88f2565e3f021f2c6f0d48fe376e002eb0c2fefa
Hu Yuanzhe <yuanzhe@iscas.ac.cn>
2014-09-09 14:44:59 +0800

    add comment in buildDes file

--- nodewebkit/backend/DataDescription/BuildDescription.js
+++ nodewebkit/backend/DataDescription/BuildDescription.js
@@ -1,2 +1,16 @@
 //BuildDescription.js
+/**
+ * @Copyright:
+ * 
+ * @Description: support API 
+ *
+ * @author: Wangfeng Xiquan Yuanzhe
+ *
+ * @Data:2014.9.9
+ *
+ * @version:0.2.1
+ **/
 
+exports.testMethod = function(arg1,arg2){
+	//TODO
+}
