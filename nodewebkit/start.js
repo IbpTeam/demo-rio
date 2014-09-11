98778581643d8fa14d47f502837a47fad0804d44
wangfeng <wangfeng@nfs.iscas.ac.cn>
2014-09-11 14:17:54 +0800

    Fix interface bugs ,not complete

--- nodewebkit/start.js
+++ nodewebkit/start.js
@@ -1,8 +1,7 @@
 var config = require("./backend/config");
 var server = require("./backend/server");
 var router = require("./backend/router");
-var msgTransfer = require("./backend/msgtransfer");
-var requestHandlers = require("./backend/requestHandlers");
+var msgTransfer = require("./backend/Transfer/msgtransfer");
 var fileHandle = require("./backend/filesHandle");
 var util = require('util');
 var os = require('os');
@@ -13,7 +12,7 @@ var handle = {}
 
 config.SERVERIP=config.getAddr();
 config.SERVERNAME = os.hostname()+'('+config.SERVERIP+')';
-msgTransfer.initServer();
+//msgTransfer.initServer();
 server.start(router.route, handle);
 
 var cp = require('child_process');
