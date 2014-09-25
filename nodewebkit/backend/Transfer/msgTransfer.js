e5eeb56019cdb0b4ebd824117f0c27f32d816421
Hu Yuanzhe <yuanzhe@iscas.ac.cn>
2014-09-25 15:33:39 +0800

    modify msgTransfer.js

--- nodewebkit/backend/Transfer/msgTransfer.js
+++ nodewebkit/backend/Transfer/msgTransfer.js
@@ -5,71 +5,69 @@ var config = require('../config');
 
 
 function initServer(){
-	var server = new WebSocketServer({port: config.MSGPORT});
+  var server = new WebSocketServer({port: config.MSGPORT});
 
-	server.on('connection',function(socket) {
+  server.on('connection',function(socket) {
+    var sRemoteAddress = socket._socket.remoteAddress;
+	var sRemotePort = socket._socket.remotePort;
 
-		var sRemoteAddress = socket._socket.remoteAddress;
-		var sRemotePort = socket._socket.remotePort;
+	console.log('messages ' + sRemoteAddress + ' : ' + sRemotePort + ' connected!');
+    socket.on('message', function(sMessage) {
+	  //console.log(sMessage)
+	  console.log('data from :' + sRemoteAddress+ ': ' + sRemotePort+ ' ' + sMessage);
+	  var oMessage = JSON.parse(sMessage);
+	  console.log('data from :' + sRemoteAddress+ ': ' + sRemotePort+ ' ' + oMessage.type);
 
-		console.log('messages ' + sRemoteAddress + ' : ' + sRemotePort + ' connected!');
-
-		socket.on('message', function(sMessage) {
-			//console.log(sMessage)
-			console.log('data from :' + sRemoteAddress+ ': ' + sRemotePort+ ' ' + sMessage);
-			var oMessage = JSON.parse(sMessage);
-			console.log('data from :' + sRemoteAddress+ ': ' + sRemotePort+ ' ' + oMessage.type);
-
-			switch(oMessage.type){
-				case 'syncRequest': {
-					dataSync.syncResponse(oMessage, sRemoteAddress);
-				}
-				break;
-				case 'syncResponse': {
-					console.log("=========================================syncStart");
-					dataSync.syncStart(oMessage, sRemoteAddress);
-				}
-				break;
-				case 'syncComplete': {
+	  switch(oMessage.type){
+		case 'syncRequest': {
+		  dataSync.syncResponse(oMessage, sRemoteAddress);
+		}
+		break;
+		case 'syncResponse': {
+		  console.log("=========================================syncStart");
+		  dataSync.syncStart(oMessage, sRemoteAddress);
+		}
+		break;
+		case 'syncComplete': {
 					dataSync.syncComplete(false, oMessage.isComplete,oMessage.deviceId,sRemoteAddress);
-				}
-				break;
-				default: {
-					console.log("this is in default switch on data");
-					//do version control stuff
-				}
-			}
-		});
-
-		socket.on('close',function(){
-			console.log('Client ' + sRemoteAddress +  ' : ' + sRemotePort + ' disconnected!');
-		});
-
-		socket.on('error',function(){
-			console.log('Unexpected Error!');
-		});
+		}
+		break;
+		default: {
+		  console.log("this is in default switch on data");
+		  //do version control stuff
+		}
+	  }
 	});
+
+	socket.on('close',function(){
+	  console.log('Client ' + sRemoteAddress +  ' : ' + sRemotePort + ' disconnected!');
+	});
+
+	socket.on('error',function(){
+	  console.log('Unexpected Error!');
+	});
+  });
 }
 
 
 function sendMsg(IP,sMessage){
-	console.log("--------------------------"+IP);
+  console.log("--------------------------"+IP);
 
-	var socket = new WebSocket('http://'+IP+':'+config.MSGPORT);
+  var socket = new WebSocket('http://'+IP+':'+config.MSGPORT);
 
-	if (IP == config.SERVERIP) {
-		console.log("Input IP is localhost!");
-		return;
-	};
+  if (IP == config.SERVERIP) {
+	console.log("Input IP is localhost!");
+	return;
+  };
 
-	socket.on('open', function() {
-		socket.send(sMessage);
-	});
+  socket.on('open', function() {
+	socket.send(sMessage);
+  });
 
-	socket.on('error',function(err){
-		console.log("Error: "+err.code+" on "+err.syscall+" !  IP : " + IP);
-		socket.close();
-	});
+  socket.on('error',function(err){
+	console.log("Error: "+err.code+" on "+err.syscall+" !  IP : " + IP);
+	socket.close();
+  });
 }
 
 exports.initServer=initServer;
