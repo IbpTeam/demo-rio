e327ee938a3a3e61b9c23f2d16fdbc1d22d658c2
Jianmin <jianmin@nfs.iscas.ac.cn>
2014-10-10 14:29:55 +0800

    Change api.js into require demo-rio

--- sdk/lib/api.js
+++ sdk/lib/api.js
@@ -1,21 +1,10 @@
 // API lib.
 console.log("head of api.js.");
-var WDC= {};
-WDC.isDebug = true;
-
-/* This function is for require api
- * Param apilist: a string array
- * Param callback: a function for callback, whose param numbers is same 
- * with the length of array.
- **/
-WDC.requireAPI = function(apilist, callback){
-  var i;
-  var apiArr = new Array(apilist.length);
-  for (i = 0; i < apilist.length; i += 1){
-    apiArr[i] = require('./lib/api/' + apilist[i]);
-  }
-  console.log("apiArr:" + apiArr);
-  setTimeout(function(){callback.apply(null, apiArr)}, 0);
+var WDC;
+try {
+  WDC=require('demo-rio');
+}catch (e){
+  console.log("Error: Can not load nodewebkit modules, so we can not use the WDC api.");
 }
 
 console.log("end of api.js.");
