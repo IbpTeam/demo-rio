0701acab163f6a2b48fe61c828b57d15622e76b5
“shuanzi” <daixiquan@gmail.com>
2014-10-17 16:53:54 +0800

    修改写.conf文件逻辑，只需传入需要修改的内容对象；添加findAllDesktopFiles()功能，buildLocalDesktopFiles()功能还未完成；改进测试页面中print()函数的功能。

--- nodewebkit/lib/api/data.js
+++ nodewebkit/lib/api/data.js
@@ -170,16 +170,16 @@ exports.getDataByUri = getDataByUri;
  */
 function openDataByUri(openDataByUriCb, uri) {
   console.log("Request handler 'openDataByUri' was called.");
-  filesHandle.openDataByUri(function(result){
-    if (result.format === "html5ppt"){
+  filesHandle.openDataByUri(function(result) {
+    if (result.format === "html5ppt") {
       console.log("open html5ppt:" + result.content);
       window.open(result.content);
-      result.content="成功打开文件" + result.content;
+      result.content = "成功打开文件" + result.content;
       setTimeout(openDataByUriCb(result), 0);
-    }else {
+    } else {
       setTimeout(openDataByUriCb(result), 0);
     }
-  },uri);
+  }, uri);
 }
 exports.openDataByUri = openDataByUri;
 
@@ -460,7 +460,7 @@ exports.readThemeConf = readThemeConf;
  *    object, content of Theme.conf after modified
  *
  *    oThem example:
- *    var oTheme = 
+ *    var oTheme =
  *    {
  *       "icontheme": {
  *           "name": "Mint-X",
@@ -533,7 +533,7 @@ exports.readWidgetConf = readWidgetConf;
  *    object, content of Widget.conf after modified
  *
  *    oWidget example:
- *    var oWidget = 
+ *    var oWidget =
  *    {
  *       "icontheme": {
  *           "name": "Mint-X",
@@ -581,8 +581,7 @@ exports.writeWidgetConf = writeWidgetConf;
  *      OnlyShowIn: GNOME;
  *      NoDisplay: true
  *      X - GNOME - Autostart - Phase: WindowManager
- *      X - GNOME - Provides: panel;
- *      windowmanager;
+ *      X - GNOME - Provides: panel;windowmanager;
  *      X - GNOME - Autostart - Notify: true
  *      X - GNOME - AutoRestart: true
  *    }
@@ -597,3 +596,34 @@ function readDesktopFile(readDesktopFileCb, sFileName) {
   desktopConf.readDesktopFile(readDesktopFileCb, sFileName);
 }
 exports.readDesktopFile = readDesktopFile;
+
+/** 
+ * @Method: writeDesktopFile
+ *    modify a desktop file
+ *
+ * @param1: callback
+ *    @result
+ *    string, a full path string,
+ *            as: '/usr/share/applications/cinnamon.desktop'
+ *
+ * @param2: sFileName
+ *    string, a file name
+ *    exmple: var sFileName = 'cinnamon.desktop';
+ *
+ * @param3: oEntries
+ *    object, this object indludes those entries that you want
+ *            to change in this desktop file.
+ *
+ *    example:
+ *    var oEntries =
+ *    {
+ *      Comment: Window management and application launching of Mac OX X,
+ *      NoDisplay: false
+ *    }
+ *
+ **/
+function writeDesktopFile(writeDesktopFileCb, sFileName, oEntries) {
+  console.log("Request handler 'writeDesktopFile' was called.");
+  desktopConf.writeDesktopFile(writeDesktopFileCb, sFileName, oEntries);
+}
+exports.writeDesktopFile = writeDesktopFile;
