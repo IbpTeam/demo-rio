4deb4bccde4834dc9b22a8bcd7b11351f8f80025
“shuanzi” <daixiquan@gmail.com>
2014-10-28 14:15:15 +0800

    在commonHandle和document中添加createData();未测试。

---
+++ nodewebkit/backend/commonHandle/commonHandle.js
@@ -0,0 +1,258 @@
+/**
+ * @Copyright:
+ *
+ * @Description: API for common handle.
+ *
+ * @author: Wangfeng Xiquan Yuanzhe
+ *
+ * @Data:2014.10.28
+ *
+ * @version:0.1.1
+ **/
+
+var http = require("http");
+var url = require("url");
+var sys = require('sys');
+var pathModule = require('path');
+var git = require("nodegit");
+var fs = require('fs');
+var fs_extra = require('fs-extra');
+var os = require('os');
+var config = require("../config");
+var dataDes = require("./desFilesHandle");
+var desktopConf = require("../data/desktop");
+var commonDAO = require("./CommonDAO");
+var resourceRepo = require("./repo");
+var device = require("./data/devices");
+var util = require('util');
+var events = require('events');
+var csvtojson = require('../csvTojson');
+var uniqueID = require("../uniqueID");
+var tagsHandles = require("./tagsHandle");
+
+var writeDbNum = 0;
+var dataPath;
+
+function getCategory(path) {
+  var pointIndex = path.lastIndexOf('.');
+  if (pointIndex == -1) {
+    var itemPostfix = "none";
+    var nameindex = path.lastIndexOf('/');
+    var itemFilename = path.substring(nameindex + 1, path.length);
+  } else {
+    var itemPostfix = path.substr(pointIndex + 1);
+    var nameindex = path.lastIndexOf('/');
+    var itemFilename = path.substring(nameindex + 1, pointIndex);
+  }
+  if (itemPostfix == 'none' ||
+    itemPostfix == 'ppt' ||
+    itemPostfix == 'pptx' ||
+    itemPostfix == 'doc' ||
+    itemPostfix == 'docx' ||
+    itemPostfix == 'wps' ||
+    itemPostfix == 'odt' ||
+    itemPostfix == 'et' ||
+    itemPostfix == 'txt' ||
+    itemPostfix == 'xls' ||
+    itemPostfix == 'xlsx' ||
+    itemPostfix == 'ods' ||
+    itemPostfix == 'zip' ||
+    itemPostfix == 'sh' ||
+    itemPostfix == 'gz' ||
+    itemPostfix == 'html' ||
+    itemPostfix == 'et' ||
+    itemPostfix == 'odt' ||
+    itemPostfix == 'pdf' ||
+    itemPostfix == 'html5ppt') {
+    return {
+      category: "Documents",
+      filename: itemFilename,
+      postfix: itemPostfix
+    };
+  } else if (itemPostfix == 'jpg' || itemPostfix == 'png') {
+    return {
+      category: "Pictures",
+      filename: itemFilename,
+      postfix: itemPostfix
+    };
+  } else if (itemPostfix == 'mp3' || itemPostfix == 'ogg') {
+    return {
+      category: "Music",
+      filename: itemFilename,
+      postfix: itemPostfix
+    };
+  } else if (itemPostfix == 'conf' || itemPostfix == 'desktop') {
+    return {
+      category: "Configuration",
+      filename: itemFilename,
+      postfix: itemPostfix
+    };
+  }
+}
+
+function copyFile(oldPath, newPath, callback) {
+  var repeat = 0;
+  console.log(newPath);
+  fs.exists(newPath, function(isExists) {
+    if (isExists) {
+      console.log('exiiiiiiiiiiiists', newPath);
+      var pointIndex = newPath.lastIndexOf('.');
+      var nameindex = newPath.lastIndexOf('/');
+      if (pointIndex == -1) {
+        var itemPostfix = "none";
+        var itemFilename = newPath.substring(nameindex + 1, newPath.length);
+      } else {
+        var itemPostfix = newPath.substr(pointIndex + 1);
+        var itemFilename = newPath.substring(nameindex + 1, pointIndex);
+      }
+      repeat++;
+      var newName = itemFilename + '(' + repeat + ')';
+      newPath = newPath.substr(0, nameindex) + newName + itemPostfix;
+      copyFile(oldPath, newPath, callback);
+    } else {
+      fs_extra.copy(oldPath, newPath, function(err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        callback('success');
+      })
+    }
+  })
+}
+
+/**
+ * @method createData
+ *    To create des file, dataBase resocrd and git commit for single data. This
+ *    method is only for single data input at a time.
+ *
+ * @param1: item
+ *    object, inlcudes all info about the input data
+ *    examplt:
+ *    var itemInfo = {
+ *               id: "",
+ *               URI: uri + "#" + category,
+ *               category: category,
+ *                is_delete: 0,
+ *                others: someTags.join(","),
+ *                filename: itemFilename,
+ *                postfix: itemPostfix,
+ *                size: size,
+ *                path: itemPath,
+ *                project: '上海专项',
+ *                createTime: ctime,
+ *                lastModifyTime: mtime,
+ *                lastAccessTime: ctime,
+ *                createDev: config.uniqueID,
+ *                lastModifyDev: config.uniqueID,
+ *                lastAccessDev: config.uniqueID
+ *              }
+ *
+ * @param2: callback
+ *    @result
+ *    string, retrieve 'success' when success
+ *
+ */
+function createData(item, callback) {
+  var itemPath = item.path;
+  var itemPostfix = item.postfix;
+  var itemFilename = item.filename;
+  var category = item.category;
+  var resourcesPath = config.RESOURCEPATH + '/' + category.toLowerCase();
+  var dest = resourcesPath + '/data/' + itemFilename + '.' + itemPostfix;
+  copyFile(itemPath, dest, function(result) {
+    if (result !== 'success') {
+      console.log(result);
+      return;
+    }
+    var itemDesPath = resourcesPath + 'des/';
+    dataDes.createItem(item, itemDesPath, function() {
+      commonDAO.createItem(item, function(err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        resourceRepo.repoAddsCommit(resourcesPath, [dest], function() {
+          callback('success');
+        });
+      })
+    });
+  });
+}
+exports.createData = createData;
+
+
+/**
+ * @method createDataAll
+ *    To create des file, dataBase resocrd and git commit for all data input. T-
+ *    -his is only for array data input.
+ *
+ * @param1: items
+ *    object, inlcudes all info about the input data
+ *    examplt:
+ *    var items = [itemInfo_1,itemInfo_2,itemInfo_3];
+ *    var itemInfo = {
+ *               id: "",
+ *               URI: uri + "#" + category,
+ *               category: category,
+ *                is_delete: 0,
+ *                others: someTags.join(","),
+ *                filename: itemFilename,
+ *                postfix: itemPostfix,
+ *                size: size,
+ *                path: itemPath,
+ *                project: '上海专项',
+ *                createTime: ctime,
+ *                lastModifyTime: mtime,
+ *                lastAccessTime: ctime,
+ *                createDev: config.uniqueID,
+ *                lastModifyDev: config.uniqueID,
+ *                lastAccessDev: config.uniqueID
+ *              }
+ *
+ * @param2: callback
+ *    @result
+ *    string, retrieve 'success' when success
+ *
+ */
+function createDataAll(items, callback) {
+  if (typeof items !== 'object') {
+    console.log('input error: items should be an object!');
+    return;
+  }
+  var count = 0;
+  var lens = items.length;
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    var itemPath = item.path;
+    var itemPostfix = item.postfix;
+    var itemFilename = item.filename;
+    var category = item.category;
+    var resourcesPath = config.RESOURCEPATH + '/' + category.toLowerCase();
+    var dest = resourcesPath + '/data/' + itemFilename + '.' + itemPostfix;
+    var allItems = [];
+    var allItemPath = [];
+    copyFile(itemPath, dest, function(result) {
+      if (result !== 'success') {
+        console.log(result);
+        return;
+      }
+      var itemDesPath = resourcesPath + 'des/';
+      dataDes.createItem(item, itemDesPath, function() {
+        allItems.push(item);
+        allItemPath.push(dest);
+        var isEnd = (count === lens);
+        if (isEnd) {
+          commonDAO.createItems(allItems, function() {
+            resourceRepo.repoAddsCommit(resourcesPath, allItemPath, function() {
+              console.log('create data all success!');
+              callback('success');
+            });
+          })
+        }
+        count++;
+      });
+    });
+  }
+}
+exports.createDataAll = createDataAll;
---
+++ nodewebkit/backend/data/document.js
@@ -0,0 +1,156 @@
+var http = require("http");
+var url = require("url");
+var sys = require('sys');
+var pathModule = require('path');
+var git = require("nodegit");
+var fs = require('fs');
+var fs_extra = require('fs-extra');
+var os = require('os');
+var config = require("../config");
+var commonDAO = require("./CommonDAO");
+var resourceRepo = require("./repo");
+var util = require('util');
+var events = require('events');
+var csvtojson = require('../csvTojson');
+var uniqueID = require("../uniqueID");
+var tagsHandles = ('../commonHandle/tagsHandles');
+var commonHandle = require('../commonHandle/commonHandle');
+
+
+/**
+ * @method createData
+ *    To create des file, dataBase resocrd and git commit for all data input. T-
+ *    -his is only for array or string data input. The proccess would be copy f-
+ *    -rst, then create the des file, after all des file done, then write into
+ *    data base, final step is commit git.
+ *
+ * @param1: items
+ *    object, an array or string of data full path.
+ *    examplt:
+ *    var items = '/home/xiquan/resource/documents/test.txt', or
+ *    var items = ['/home/xiquan/resource/documents/test1.txt',
+ *                 '/home/xiquan/resource/documents/test2.txt'
+ *                 '/home/xiquan/resource/documents/test3.txt'].
+ *
+ * @param2: callback
+ *    @result
+ *    string, retrieve 'success' when success
+ *
+ */
+function createData(items, callback) {
+  if (typeof items === 'string') {
+    fs.stat(items, function(err, stat) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      var mtime = stat.mtime;
+      var ctime = stat.ctime;
+      var size = stat.size;
+      var cate = commonHandle.getCategory(itemPath);
+      var category = 'Documents';
+      var itemFilename = cate.filename;
+      var itemPostfix = cate.postfix;
+      var someTags = tagsHandles.getTagsByPath(itemPath);
+      var resourcesPath = config.RESOURCEPATH + '/' + category;
+      uniqueID.getFileUid(function(uri) {
+        var itemInfo = {
+          id: "",
+          URI: uri + "#" + category,
+          category: category,
+          is_delete: 0,
+          others: someTags.join(","),
+          filename: itemFilename,
+          postfix: itemPostfix,
+          size: size,
+          path: itemPath,
+          project: '上海专项',
+          createTime: ctime,
+          lastModifyTime: mtime,
+          lastAccessTime: ctime,
+          createDev: config.uniqueID,
+          lastModifyDev: config.uniqueID,
+          lastAccessDev: config.uniqueID
+        };
+        commonHandle.createData(itemInfo, function(result) {
+          if (result === 'success') {
+            callback(null, result);
+          } else {
+            var _err = 'createData: commonHandle createData error!';
+            console.log('createData error!');
+            callback(_err, null);
+          }
+        })
+      })
+    })
+  } else if (typeof items === 'object') {
+    if (!items.length) {
+      console.log('create data input error!');
+      var _err = 'createData: items should be an array!';
+      callback(_err, null);
+    } else {
+      var itemInfoAll[];
+      var count = 0;
+      var lens = items.length;
+      for (var i = 0; i < lens; i++) {
+        var item = items[i];
+        fs.stat(items, function(err, stat) {
+          if (err) {
+            console.log(err);
+            var _err = err;
+            callback(_err, null);
+          } else {
+            var mtime = stat.mtime;
+            var ctime = stat.ctime;
+            var size = stat.size;
+            var cate = commonHandle.getCategory(itemPath);
+            var category = 'Documents';
+            var itemFilename = cate.filename;
+            var itemPostfix = cate.postfix;
+            var someTags = tagsHandles.getTagsByPath(itemPath);
+            var resourcesPath = config.RESOURCEPATH + '/' + category;
+            uniqueID.getFileUid(function(uri) {
+              var itemInfo = {
+                id: "",
+                URI: uri + "#" + category,
+                category: category,
+                is_delete: 0,
+                others: someTags.join(","),
+                filename: itemFilename,
+                postfix: itemPostfix,
+                size: size,
+                path: itemPath,
+                project: '上海专项',
+                createTime: ctime,
+                lastModifyTime: mtime,
+                lastAccessTime: ctime,
+                createDev: config.uniqueID,
+                lastModifyDev: config.uniqueID,
+                lastAccessDev: config.uniqueID
+              };
+              itemInfoAll.push(itemInfo);
+              var isEnd = (count === lens);
+              if (isEnd) {
+                commonHandle.createDataAll(itemInfoAll, function(result) {
+                  if (result === 'success') {
+                    callback(null, result);
+                  } else {
+                    var _err = 'createData: commonHandle createData all error!';
+                    console.log('createData error!');
+                    callback(_err, null);
+                  }
+                })
+              }
+              count++;
+            })
+          }
+        })
+      }
+    }
+  } else {
+    console.log('input error: items is undefined!');
+    var _err = 'createData: input error';
+    callback(_err, null);
+  }
+}
+exports.createData = createData;
