051ba203c7800425945de67b32e80935763b0f36
rtty122333 <wangbo7799@gmail.com>
2014-10-30 20:07:02 +0800

    add file transfer client functions

---
+++ nodewebkit/backend/IM/file-trans/clientTest.js
@@ -0,0 +1,21 @@
+var transferFile = require('./transferFile');
+var fileTransferServer = require('./fileTransferServer');
+var crypto = require('crypto');
+var input = __dirname + '/geju.mkv';
+//var input = __dirname + '/file1.txt'
+var input0 = __dirname + '/JsFormat.tar.gz'
+var fileServer;
+var IP = '192.168.160.66';
+var buf = input.split('/');
+var name = buf[buf.length - 1];
+var buf0 = input0.split('/');
+var name0 = buf0[buf0.length - 1];
+console.log(__dirname);
+console.log(name0);
+
+transferFile.transferFile(IP, input, name, function() {
+	console.log('transferFiletransferFiletransferFiletransferFiletransferFile');
+});
+transferFile.transferFile(IP, input0, name0, function() {
+	console.log('transferFiletransferFiletransferFiletransferFiletransferFile');
+});
