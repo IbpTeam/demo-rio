1fba6ef7faf2d77f1b3acc91e57f99e56906e035
Jianmin <jianmin@nfs.iscas.ac.cn>
2014-11-06 12:37:19 +0800

    Upgrade the remote api implement. support the function level from the file level.

--- nodewebkit/lib/api/mix_remote.js
+++ nodewebkit/lib/api/mix_remote.js
@@ -1,32 +1,13 @@
 //This file is generated manually.
-define(function(){
-  var o={};
-  function sendrequest(a, ar){
-    var sd = {};
-    var cb=ar.shift();
-    sd.api = a;sd.args = ar;
-    $.ajax({
-      url: "/callapi", type: "post", contentType: "application/json;charset=utf-8", dataType: "json",
-      data: JSON.stringify(sd),
-      success: function(r) {setTimeout(cb.apply(null,r), 0);},
-      error: function(e) {throw e;}
-    });
-  }
-  o.getHello = function(){
-    sendrequest("mix.getHello", Array.prototype.slice.call(arguments));
-  }
-  o.getHello2 = function(){
-    sendrequest("mix.getHello2", Array.prototype.slice.call(arguments));
-  }
-  o.getHello3 = function(){
-    sendrequest("mix.getHello3", Array.prototype.slice.call(arguments));
-  }
-  o.openDev = function(callback){
-    console.log("openDev is not supported now.");
-    setTimeout(callback(false), 0);
-  }
-  o.isLocal = function(callback){
-    setTimeout(callback(false), 0);
-  }
-  return o;
-})
+//@remote openDev
+function openDev = function(callback){
+  console.log("openDev is not supported now.");
+  setTimeout(callback(false), 0);
+}
+exports.openDev=openDev;
+
+//@remote isLocal
+function isLocal = function(callback){
+  setTimeout(callback(false), 0);
+}
+exports.isLocal=isLocal;
