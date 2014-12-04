72c4cbe909900c26d137fdfcbf27dbfcb88ad2e9
WangYu <wangyu@nfs.iscas.ac.cn>
2014-12-04 17:46:00 +0800

    fix conflict

--- newdatamgr/js/contact.js
+++ newdatamgr/js/contact.js
@@ -12,15 +12,22 @@ var Contact = Class.extend({
 
   setContactsList:function(){
     var _this = this;
-    DataAPI.getAllDataByCate(function(contact_json){
-      _this._contacts = contact_json;
+    DataAPI.getAllDataByCate(function(contact_json_){
+      console.log("contact_json_=====", contact_json_);
+      _this._contacts = contact_json_;
       var family_name_json = {};
       for(var i = 0; i < _this._contacts.length; i ++){
         var family_name = _this._contacts[i]['name'][0];
         if(family_name_json.hasOwnProperty(family_name)){
-          family_name_json[family_name].push(_this._contacts[i]['name']);
+          family_name_json[family_name].push({
+            name: _this._contacts[i]['name'],
+            id: i
+          });
         } else {
-          family_name_json[family_name] = [_this._contacts[i]['name']];
+          family_name_json[family_name] = [{
+            name: _this._contacts[i]['name'],
+            id: i
+          }];
         }
       }
       for(var i in family_name_json){
@@ -35,13 +42,25 @@ var Contact = Class.extend({
         for(var j = 0; j < family_name_json[i].length; j ++){
           var _name = $('<li>', {
             'class':'a-name',
-            'text': family_name_json[i][j]
+            'text': family_name_json[i][j]['name'],
+            'id': family_name_json[i][j]['id']
           });
           _ul.append(_name);
         }
         _this._contactsList.append(_ul);
       }
+      _this.bindAction();
     }, 'Contact');
+  },
+
+  bindAction: function(){
+    $('.p-name').on('click', function(){
+      alert(this.id);
+    });
+  },
+
+  setHead: function(){
+
   },
 
   attach:function($parent_){
