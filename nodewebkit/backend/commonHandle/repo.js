3bfd45d5d10a4205fb27a29cb11634c6f4323f55
wangfeng <wangfeng@nfs.iscas.ac.cn>
2014-12-05 10:58:34 +0800

    修复联系人删除不能恢复操作的Bug

--- nodewebkit/backend/commonHandle/repo.js
+++ nodewebkit/backend/commonHandle/repo.js
@@ -7,8 +7,8 @@ var transfer = require('../Transfer/msgTransfer');
 
 var repos=[
   {name:"contactDes",status:"empty"},
-  {name:"desktop",status:"empty"},
-  {name:"desktopDes",status:"empty"},
+//  {name:"desktop",status:"empty"},
+//  {name:"desktopDes",status:"empty"},
   {name:"document",status:"empty"},
   {name:"documentDes",status:"empty"},
   {name:"music",status:"empty"},
@@ -22,7 +22,7 @@ var repos=[
 ];
 var desRepos=[
   {name:"contactDes",status:"empty"},
-  {name:"desktopDes",status:"empty"},
+ // {name:"desktopDes",status:"empty"},
   {name:"documentDes",status:"empty"},
   {name:"musicDes",status:"empty"},
   {name:"otherDes",status:"empty"},
@@ -40,7 +40,6 @@ exports.repoInit = function(repoPath, callback) {
     console.log("Repo init : " + repo);
     var exec = require('child_process').exec;
     var comstr = 'cd ' + repoPath + ' && git add . && git commit -m "On device ' + config.SERVERNAME + ' #Init resource#"';
-    console.log("runnnnnnnnnnnnnnnnnnnnnnnnnn" + comstr);
     exec(comstr, function(error, stdout, stderr) {
       if (error) {
         callback("Git init error");
@@ -68,7 +67,6 @@ function repoCommit(repoPath, files, commitID, op, callback) {
   var fileInfo = '"file":["' + files.join('","') + '"]';
   var commitLog = '{' + relateCommit + deviceInfo + ',' + opInfo + ',' + fileInfo + '}';
   comstr = comstr + " && git commit -m '" + commitLog + "'";
-  console.log("runnnnnnnnnnnnnnnnnnnnnnnnnn:\n" + comstr);
   exec(comstr, function(error, stdout, stderr) {
     if (error) {
       console.log("Git add error");
@@ -99,9 +97,9 @@ function repoResetCommit (repoPath, file, commitID,oriOp, callback) {
   console.log("runnnnnnnnnnnnnnnnnnnnnnnnnn:\n" + comstr);
   exec(comstr, function(error, stdout, stderr) {
     if (error) {
-      console.log("Git change error", error, stdout);
+      console.log("Git revert error", error, stdout);
     } else {
-      console.log("Git change success");
+      console.log("Git revert success");
       callback(null,'success');
       transfer.syncOnlineReq(repoPath);
     }
@@ -373,7 +371,6 @@ function getGitLog(repoPath, callback) {
         commitLog[logItem[0]] = tmplogItem;
       }
     }
-    console.log(commitLog);
     callback(null, commitLog);
   })
 }
@@ -485,7 +482,6 @@ exports.repoRenameCommit = function(sOrigin, sNew, repoPath, desRepoPath, callba
           console.log("Git change error", error, stdout);
           return
         }
-        console.log("Git change success");
         callback(null, 'success');
       });
     })
