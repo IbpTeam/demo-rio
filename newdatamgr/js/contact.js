a16f67b90f3ecdf3cbd825475504b8ff515b82c4
WangYu <wangyu@nfs.iscas.ac.cn>
2014-12-05 16:02:05 +0800

    finish general UI of contacts

--- newdatamgr/js/contact.js
+++ newdatamgr/js/contact.js
@@ -1,6 +1,6 @@
 var Contact = Class.extend({
   init:function(){
-    this._contacts = ['Aaliyah', 'Abagail', 'Abi', 'Brian', 'Charies', 'Cindy'];
+    this._contacts = [];
     this._ContactContainer = $('<div>',{
       'id': 'contact-container'
     })
@@ -8,12 +8,20 @@ var Contact = Class.extend({
       'id': 'contact-left'
     });
     this._ContactContainer.append(this._contactsList);
+    this._contactHead = $('<div>', {
+      'id': 'contact-head'
+    });
+    this._ContactContainer.append(this._contactHead);
+    this._defaultPhoto = 'img/headphoto.png';
+    this._contactDetails = $('<div>', {
+      'id': 'contact-detail'
+    });
+    this._ContactContainer.append(this._contactDetails);
   },
 
   setContactsList:function(){
     var _this = this;
     DataAPI.getAllDataByCate(function(contact_json_){
-      console.log("contact_json_=====", contact_json_);
       _this._contacts = contact_json_;
       var family_name_json = {};
       for(var i = 0; i < _this._contacts.length; i ++){
@@ -41,7 +49,7 @@ var Contact = Class.extend({
         _ul.append(_first);
         for(var j = 0; j < family_name_json[i].length; j ++){
           var _name = $('<li>', {
-            'class':'a-name',
+            'class':'li-name',
             'text': family_name_json[i][j]['name'],
             'id': family_name_json[i][j]['id']
           });
@@ -49,18 +57,82 @@ var Contact = Class.extend({
         }
         _this._contactsList.append(_ul);
       }
+      _this.setHead(_this._contacts[0]);
+      _this.setDetails(_this._contacts[0]);
       _this.bindAction();
     }, 'Contact');
   },
 
   bindAction: function(){
-    $('.a-name').on('click', function(){
-      alert(this.id);
+    var _this = this;
+    $('.li-name').on('click', function(){
+      _this.removeHead();
+      _this.removeDetails();
+      _this.setHead(_this._contacts[this.id]);
+      _this.setDetails(_this._contacts[this.id]);
     });
   },
 
-  setHead: function(){
+  setHead: function(contact_){
+    var _headPhotoPath = '';
+    if(contact_['photoPath'] != null){
+      _headPhotoPath = contact_['photoPath'];
+    } else{
+      _headPhotoPath = this._defaultPhoto;
+    }
+    var _photoDiv = $('<div>', {
+      'class' : 'div-headphoto'
+    });
+    var _photo = $('<img>', {
+      'class': 'img',
+      'src': _headPhotoPath
+    });
+    _photoDiv.append(_photo);
+    var _nameDiv = $('<div>', {
+      'class': 'div-name',
+      'text': contact_['name']
+    });
+    var _tags = ['Family', 'mather', 'Friends'];
+    this._contactHead.append(_photoDiv);
+    this._contactHead.append(_nameDiv);
+  },
 
+  setDetails: function(contact_){
+    var _ul = $('<ul>', {
+      'class':'ul-details'
+    });
+    for(var key in contact_){
+      if(key == 'URI') continue;
+      var _li = $('<li>',{
+        'class': 'li-details'
+      });
+      var _keyDiv = $('<div>', {
+        'class': 'div-key',
+        'text': key
+      });
+      var _valueDiv = $('<div>', {
+        'class': 'div-value',
+        'text': contact_[key]
+      });
+      _li.append(_keyDiv);
+      _li.append(_valueDiv);
+      _ul.append(_li);
+    }
+    this._contactDetails.append(_ul);
+  },
+
+  removeHead: function(){
+    var _list = this._contactHead.children();
+    if(_list.length != 0){
+      _list.remove();
+    }
+  },
+
+  removeDetails: function(){
+    var _list = this._contactDetails.children();
+    if(_list.length != 0){
+      _list.remove();
+    }
   },
 
   attach:function($parent_){
