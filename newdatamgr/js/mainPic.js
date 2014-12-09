64d20b11546ec04a5f758caba5a1546df2aaa74b
WangYu <wangyu@nfs.iscas.ac.cn>
2014-12-09 14:48:25 +0800

    save

--- newdatamgr/js/mainPic.js
+++ newdatamgr/js/mainPic.js
@@ -3,12 +3,17 @@ var MainPicView = Class.extend({
     this._picContainer = $('<div>',{
       'id': 'pic-container'
     });
-    this._picData={
-      'path': 'img/show.jpg',
-      'text': 'heaven',
-      'date': ' 2014-10-31' 
-    }
-    this.setPicture(this._picData);
+    var _this = this;
+    DataAPI.getRecentAccessData(function(err_, picture_json_){
+      for(var i = 0; i < picture_json_.length; i++){
+        _this._picData={
+          'path': picture_json_[i]['path'],
+          'text': picture_json_[i]['filename'],
+          'date': picture_json_[i]['createTime'].substring(0, picture_json_[i]['createTime'].lastIndexOf('T')) 
+        }
+        _this.setPicture(_this._picData);
+      }
+    }, 'picture', 1);
   },
 
   attach:function($parent_){
