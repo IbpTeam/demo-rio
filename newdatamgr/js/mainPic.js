8bb8140709244e555850654e66fd84907f8b4cc5
WangYu <wangyu@nfs.iscas.ac.cn>
2014-12-09 15:10:38 +0800

    change pic creat time format

--- newdatamgr/js/mainPic.js
+++ newdatamgr/js/mainPic.js
@@ -6,10 +6,11 @@ var MainPicView = Class.extend({
     var _this = this;
     DataAPI.getRecentAccessData(function(err_, picture_json_){
       for(var i = 0; i < picture_json_.length; i++){
+        var _date = new Date(picture_json_[i]['createTime']);
         _this._picData={
           'path': picture_json_[i]['path'],
           'text': picture_json_[i]['filename'],
-          'date': picture_json_[i]['createTime'].substring(0, picture_json_[i]['createTime'].lastIndexOf('T')) 
+          'date': _date.toDateString()
         }
         _this.setPicture(_this._picData);
       }
