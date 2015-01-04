b0c8c71d01ad693091652424cfd696da7e0c52ec
“shuanzi” <daixiquan@gmail.com>
2015-01-04 09:21:17 +0800

    修复联系人改photoPath问题

--- nodewebkit/backend/data/contacts.js
+++ nodewebkit/backend/data/contacts.js
@@ -133,7 +133,7 @@ function getAllContacts(getAllCb) {
           name: each.name,
           sex: each.sex,
           age: each.age,
-          photoPath: each.path,
+          photoPath: each.photoPath,
           phone: each.phone,
           email: each.email,
           others: each.others
