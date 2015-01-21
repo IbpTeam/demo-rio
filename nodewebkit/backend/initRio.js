353d7aeab9598d009bea4df6e6e5dd5177550eba
xifei <xifei@nfs.iscas.ac.cn>
2015-01-21 15:37:31 +0800

    remove unused comment.

--- nodewebkit/backend/initRio.js
+++ nodewebkit/backend/initRio.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var cp = require('child_process');
 var config = require('./config');
-var fullPath = config.BASEPATH;//path.join("/home", process.env['USER'], ".demo-rio");
+var fullPath = config.BASEPATH;
 
 if (!fs.existsSync(fullPath)) {
 	cp.exec('bash /usr/share/webde/init/init_rio.sh && bash /usr/share/webde/init/init_database.sh');
