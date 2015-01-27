e9eef061f14c1dacae14a8847184151ba685900b
CannedFish <lianggy0719@126.com>
2015-01-27 16:36:24 +0800

    modify comments

--- nodewebkit/lib/api/notifier.js
+++ nodewebkit/lib/api/notifier.js
@@ -1,12 +1,16 @@
 var notifier = require('../../backend/notifier/notifier.js');
 
 /**
- * Show a system level notification
- * getInitInfoCB: function(err, response)
- *    err: error discription or null
- *    response: 
- * title: the title of notification
- * msg: the content of notification
+ * @method notify
+ *    Show a system level notification
+ *
+ * @param notifyCB: function(err, response)
+ *    @param err: error discription or null
+ *    @param response: not known temporary
+ *
+ * @param title: the title of notification
+ *
+ * @param msg: the content of notification
  */
 exports.notify = function(notifyCB, title, msg) {
   notifier.notify(title, msg, notifyCB);
