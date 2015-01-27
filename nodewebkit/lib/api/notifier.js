812fe1cc82b6ea34dab9f6970a757c885cf5e6db
CannedFish <lianggy0719@126.com>
2015-01-27 16:17:38 +0800

    add some comments

--- nodewebkit/lib/api/notifier.js
+++ nodewebkit/lib/api/notifier.js
@@ -1,5 +1,13 @@
 var notifier = require('../../backend/notifier/notifier.js');
 
+/**
+ * Show a system level notification
+ * getInitInfoCB: function(err, response)
+ *    err: error discription or null
+ *    response: 
+ * title: the title of notification
+ * msg: the content of notification
+ */
 exports.notify = function(notifyCB, title, msg) {
   notifier.notify(title, msg, notifyCB);
 }
