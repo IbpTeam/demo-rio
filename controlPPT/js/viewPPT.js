06a0e338b9963b6f53e0c58238a4265afd77a1c2
zhoukai <zhoukai@nfs.iscas.ac.cn>
2015-01-29 09:36:03 +0800

    完善二维码地址的生成方式，更换图标（与数据管理器一致

--- controlPPT/js/viewPPT.js
+++ controlPPT/js/viewPPT.js
@@ -12,7 +12,7 @@ function cb_get_ppt_data(data_json){
 
     		var _div1 = $('<div class = "file" data-uri = "' + data_json[i].URI + '"></div>');
     		var _div2 = $('<div class = "icon"></div>');
-    		var _img = $('<img src = "icons/powerpoint.png">');
+    		var _img = $('<img src = "icons/Powerpoint.png">');
     		var _div3 = $('<div class = "name" id = "'+ data_json[i].filename +'">' + data_json[i].filename +
     			'</div>');
     		pl.append(_div1);
@@ -128,7 +128,11 @@ function bindEvent(){
         WDC.requireAPI(['data'],function(data){
             console.log("data"+data);
             data.getServerAddress(function(serverAddr_){
-                var _hostLink = 'http://' + serverAddr_.ip + ':' + serverAddr_.port + '/callapp/app-controlPPT/index.html#';
+                if(window !== top){
+                    var _hostLink = 'http://' + serverAddr_.ip + ':' + serverAddr_.port +'/callapp/'+WDC.AppID+ '/index.html';
+                }else{
+                    var _hostLink = 'http://' + serverAddr_.ip + ':' + serverAddr_.port + '/index.html';
+                }
                 console.log("_hostLink of the qrcode is " + _hostLink);
                 _qrcodeContainer.children('.qrcode-content') .qrcode({
                     text: _hostLink,
