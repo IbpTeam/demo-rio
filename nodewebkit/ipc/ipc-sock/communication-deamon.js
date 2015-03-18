015febf82822ea953f3e1fc85928138e3c4b802d
LiangGuanyu <lianggy0719@126.com>
2015-03-18 19:03:56 +0800

    modify some function

--- nodewebkit/ipc/ipc-sock/communication-deamon.js
+++ nodewebkit/ipc/ipc-sock/communication-deamon.js
@@ -5,56 +5,109 @@ var net = require('net');
 // Elements of this cache are objects like: {
 //  val: the real value
 //  age: the age of this value
-// }
-function Cache(capacity) {
+// }, by default.
+function Cache(capacity, stratagy) {
   this._capacity = capacity || 10;
   this._size = 0;
   this._c = new Array(capacity);
+  if(typeof stratagy !== 'undefined') {
+    if(typeof stratagy.init === 'undefined' 
+        || typeof stratagy.update === 'undefined'
+        || typeof stratagy.repTarget === 'undefined') {
+      this._init = this._born;
+      this._update = this._older;
+      this._repTarget = this._findOldest;
+    } else {
+      this._init = stratagy.init;
+      this._update = stratagy.update;
+      this._repTarget = stratagy.repTarget;
+    }
+  }
 }
 
 Cache.prototype.get = function(key) {
   if(typeof this._c[key] !== 'undefined') {
     // TODO: change the corresponding LRU perameter
-    this._c[key].age = 0;
-    for(var k in this._c) {
-      this._c[k].age++;
-    }
+    this._update(key, this._c);
     return this._c[key].val;
   }
   throw 'Not found';
 }
 
 Cache.prototype.set = function(key, val) {
-  // TODO: remove LRU obj, if cache has full
+  // By default, remove the LRU obj if cache has full
   if(this._size == this._capacity) {
-    var oKey = this._findOldest();
-    // TODO: release the corresponding resource
-    this._c[oKey] = null;
-    delete this._c[oKey];
+    this._repTarget(this._c);
     this._size--;
   }
   this._c[key] = {
-    age: 1,
     val: val
   };
+  this._init(key, this._c);
   this._size++;
 }
 
-Cache.prototype._findOldest = function() {
+Cache.prototype._born = function(key, list) {
+  list[key].age = 1;
+}
+
+Cache.prototype._older = function(key, list) {
+  list[key].age = 0;
+  for(var k in list) {
+    list[k].age++;
+  }
+}
+
+Cache.prototype._findOldest = function(list) {
   var oldest = 0, oKey = null;
-  for(var k in this._c) {
-    if(this._c[k].age > oldest) {
-      oldest = this._c[k];
+  for(var k in list) {
+    if(list[k].age > oldest) {
+      oldest = list[k].age;
       oKey = k;
     }
   }
-  return oKey;
+  // release the corresponding resource
+  try {
+    list[key].val.release();
+    list[key] = null;
+    delete list[key];
+  } catch(e) {
+    console.log(e);
+  }
 }
 
 function PeerEnd() {
   this._port = 56765;
   this._svrObj = new Cache(20);
   this._svrList = [];
+  this._connList = new Cache(20, {
+    init: function(key, list) {
+      list[key].timer = setTimeout(function() {
+        try {
+          list[key].val.release();
+          list[key] = null;
+          delete list[key];
+        } catch(e) {
+          console.log(e);
+        }
+      }, 120000);
+    },
+    update: function(key, list) {
+      clearTimeout(list[key].timer);
+      list[key].timer = setTimeout(function() {
+        try {
+          list[key].val.release();
+          list[key] = null;
+          delete list[key];
+        } catch(e) {
+          console.log(e);
+        }
+      }, 120000);
+    },
+    repTarget: function(list) {
+      // need do nothing
+    }
+  })
 
   this._init();
 }
@@ -87,9 +140,13 @@ PeerEnd.prototype._accept = function(cliSock) {
 }
 
 PeerEnd.prototype._packet = function(content) {
+  // TODO: put content into a data packet
+  return content;
 }
 
 PeerEnd.prototype._unpack = function(packet) {
+  // TODO: get content from data packet
+  return packet;
 }
 
 PeerEnd.prototype._dispatcher = function(msg) {
@@ -98,15 +155,26 @@ PeerEnd.prototype._dispatcher = function(msg) {
 
 // TODO: maintain a connection for seconds, close idle connections
 //  which have none communication with the peer out of time.
-PeerEnd.prototype._getConnection = function(ip, callback) {
-  var cb = callback || function() {},
-      client = net.connect({
-        host: ip,
-        port: this._port
-      }, function() {
-        cb(client);
-      });
-  client.on('data', function(data) {});
+PeerEnd.prototype._getConnection = function(ip) {
+  var client;
+  try {
+    client = this._connList.get(ip);
+  } catch(e) {
+    client = net.connect({
+      host: ip,
+      port: this._port
+    }, function() {
+      // TODO: connected successfully
+    });
+    client.setKeepAlive(true);
+    client.release = client.destroy;
+    this._connList.set(ip, client);
+    client.on('data', function(data) {
+      // TODO: handle data from server
+    }).on('end', function() {
+      // TODO: disconnected from server
+    });
+  }
   return client;
 }
 
@@ -115,6 +183,9 @@ PeerEnd.prototype.send = function(dstAddr, content) {
   if(net.isIP(dstAddr) == 0)
     return 'Invalid IP address';
   var conn = this._getConnection(dstAddr);
+  conn.write(this._packet(content), function() {
+    // TODO: do sth after sending packet
+  });
 }
 
 // TODO: API for clients to register services
@@ -122,4 +193,10 @@ PeerEnd.prototype.register = function(svrName, svrAddr) {}
 
 // TODO: API for clients to unregister services
 PeerEnd.prototype.unregister = function(svrName) {}
+
+function main() {
+  // TODO: register PeerEnd on local IPC framework to be a service
+}
+
+main();
 
