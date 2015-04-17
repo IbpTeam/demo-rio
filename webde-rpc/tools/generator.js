967d85dd5f6d7026458a2eb91d63ff02f1e2cebe
CannedFish <lianggy0719@126.com>
2015-04-17 17:06:25 +0800

    add new key word for generator

--- webde-rpc/tools/generator.js
+++ webde-rpc/tools/generator.js
@@ -153,6 +153,9 @@ function buildProxy(filename, initObj, ifaces, remote) {
       + (remote ? "" : EVENTHANDLER)
       + '}\n');
     for(var i = 0; i < ifaces.length; ++i) {
+      // console.log(ifaces[i].show);
+      if((ifaces[i].show == 'l' && remote) || (ifaces[i].show == 'r' && !remote))
+        continue;
       outputFile.push("Proxy.prototype." + ifaces[i].name + " = function(" 
           + ifaces[i].in.join(', ')
           + (ifaces[i].in.length == 0 ? "" : ", ") + "callback) {\n"
