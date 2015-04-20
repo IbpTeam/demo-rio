dd7705a0bd1b46a8cc457967c2ae0c2a5f9cb7ee
LiangGuanyu <lianggy0719@126.com>
2015-04-20 10:36:00 +0800

    add a tool for extracting interfaces

---
+++ webde-rpc/tools/extract.js
@@ -0,0 +1,40 @@
+// extract interfaces from JS objects
+//
+if(process.argv.length < 3)
+  return console.log('Usage: node extract.js ${List_of_Your_JS_Module_File}');
+
+var path = require('path'),
+    fs = require('fs'),
+    json4line = require('../../sdk/utils/json4line');
+
+function doExtract(mPath) {
+  var obj = require(mPath),
+      svrName = path.basename(mPath, '.js')
+      output = {
+        'package': undefined,
+        'service': svrName,
+        'path': undefined,
+        'remote': 'false',
+        'interfaces': []
+      };
+  console.log('Extracting', mPath, '...');
+  for(var key in obj) {
+    // if(typeof key.obj === 'function') {
+      // console.log(key.obj);
+    // }
+    output.interfaces.push({
+      'name': key,
+      'in': [],
+      'show': undefined
+    });
+  }
+  json4line.writeJSONFile(svrName + 'Iface', output, function(err) {
+    if(err) console.log(err);
+  });
+}
+
+(function main(modules) {
+  for(var i = 0; i < modules.length; ++i) {
+    doExtract(path.resolve(modules[i]));
+  }
+})(process.argv.slice(2));
