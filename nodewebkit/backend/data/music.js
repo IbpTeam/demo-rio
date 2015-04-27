eb9b667e1197a435270063fe18b95b513b28bd23
“shuanzi” <daixiquan@gmail.com>
2015-04-27 14:59:23 +0800

    将各类型的createData中重复部分提取出来，减少了大部分的代码复用。

--- nodewebkit/backend/data/music.js
+++ nodewebkit/backend/data/music.js
@@ -28,6 +28,7 @@ var csvtojson = require('../csvTojson');
 var uniqueID = require("../uniqueID");
 var tagsHandle = require('../commonHandle/tagsHandle');
 var commonHandle = require('../commonHandle/commonHandle');
+var rdfHandle = require('../commonHandle/rdfHandle');
 var dataDes = require('../commonHandle/desFilesHandle');
 
 
@@ -143,159 +144,36 @@ exports.readId3FromMp3 = readId3FromMp3;
  *
  */
 function createData(items, callback) {
-  if (items == [] || items == "") {
-    return callback(null, 'no Music');
-  }
-  if (typeof items == 'string') {
-    fs.stat(items, function(err, stat) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      var mtime = stat.mtime;
-      var ctime = stat.ctime;
-      var size = stat.size;
-      var cate = utils.getCategoryByPath(items);
-      var category = CATEGORY_NAME;
-      var itemFilename = cate.filename;
-      var itemPostfix = cate.postfix;
-      var someTags = tagsHandle.getTagsByPath(items);
-      var resourcesPath = config.RESOURCEPATH + '/' + category;
-      uniqueID.getFileUid(function(uri) {
-        readId3FromMp3(items,function(err,tags){
-          console.log("read mp3 "+items);
-          console.log(err);
-          console.log(tags);
-          var itemInfo = {
-            id: null,
-            URI: uri + "#" + category,
-            category: category,
-            others: someTags.join(","),
-            filename: itemFilename,
-            postfix: itemPostfix,
-            size: size,
-            path: items,
-            format:tags.format,
-            bit_rate:tags.bit_rate,
-            frequency:tags.frequency,
-            track:tags.track,
-            TDRC:tags.TDRC,
-            APIC:tags.APIC,
-            TALB:tags.TALB,
-            TPE1:tags.TPE1,
-            TIT2:tags.TIT2,
-            TXXX:tags.TXXX,
-            COMM:tags.COMM,
-            createTime: ctime,
-            lastModifyTime: mtime,
-            lastAccessTime: ctime,
-            createDev: config.uniqueID,
-            lastModifyDev: config.uniqueID,
-            lastAccessDev: config.uniqueID
-          };
-          console.log(itemInfo);
-          commonHandle.createData(itemInfo, function(result, resultFile) {
-            if (result === 'success') {
-              callback(null, resultFile);
-            } 
-            else {
-              var _err = 'createData: commonHandle createData error!';
-              console.log('createData error!');
-              callback(_err, null);
-            }
-          });
-        });
-      });
-    });
-  } else if (typeof items == 'object') {
-    if (!items.length) {
-      console.log('create data input error!');
-      var _err = 'createData: items should be an array!';
-      callback(_err, null);
-    } else {
-      var itemInfoAll = [];
-      var count = 0;
-      var lens = items.length;
-      for (var i = 0; i < lens; i++) {
-        var item = items[i];
-        (function(_item) {
-          fs.stat(_item, function(err, stat) {
-            if (err) {
-              console.log(err);
-              var _err = err;
-              callback(_err, null);
-            } else {
-              var mtime = stat.mtime;
-              var ctime = stat.ctime;
-              var size = stat.size;
-              var cate = utils.getCategoryByPath(_item);
-              var category = CATEGORY_NAME;
-              var itemFilename = cate.filename;
-              var itemPostfix = cate.postfix;
-              var someTags = tagsHandle.getTagsByPath(_item);
-              var resourcesPath = config.RESOURCEPATH + '/' + category;
-              uniqueID.getFileUid(function(uri) {
-                readId3FromMp3(_item,function(err,tags){
-                  console.log("read mp3 "+_item);
-                  console.log(err);
-                  console.log(tags);
-                  var itemInfo = {
-                    id: null,
-                    URI: uri + "#" + category,
-                    category: category,
-                    others: someTags.join(","),
-                    filename: itemFilename,
-                    postfix: itemPostfix,
-                    size: size,
-                    path: _item,
-                    format:tags.format,
-                    bit_rate:tags.bit_rate,
-                    frequency:tags.frequency,
-                    track:tags.track,
-                    TDRC:tags.TDRC,
-                    APIC:tags.APIC,
-                    TALB:tags.TALB,
-                    TPE1:tags.TPE1,
-                    TIT2:tags.TIT2,
-                    TXXX:tags.TXXX,
-                    COMM:tags.COMM,
-                    createTime: ctime,
-                    lastModifyTime: mtime,
-                    lastAccessTime: ctime,
-                    createDev: config.uniqueID,
-                    lastModifyDev: config.uniqueID,
-                    lastAccessDev: config.uniqueID
-                  };
-                  console.log(itemInfo);
-                  itemInfoAll.push(itemInfo);
-                  var isEnd = (count === lens - 1);
-                  if (isEnd) {
-                    commonHandle.createDataAll(itemInfoAll, function(err, result) {
-                      if (err) {
-                        var _err = 'createData: commonHandle createData all error!';
-                        console.log('createData error!');
-                        return callback(_err, null);
-                      }
-                      callback(null, result);
-                    });
-                  }
-                  count++;
-                });
-              });
-            }
-          });
-        })(item)
-      }
+  commonHandle.dataStore(items, extraInfo, function(err) {
+    if (err) {
+      return callback(err);
     }
-  } 
-  else {
-    console.log('input error: items is undefined!');
-    var _err = 'createData: input error';
-    callback(_err, null);
-  }
+    callback();
+  })
 }
 exports.createData = createData;
 
+function extraInfo(item, callback) {
+  readId3FromMp3(item, function(err, tags) {
+    if (err) {
+      return callback(err);
+    }
+    var _extra = {
+      format: tags.format,
+      bit_rate: tags.bit_rate,
+      frequency: tags.frequency,
+      track: tags.track,
+      TDRC: tags.TDRC,
+      APIC: tags.APIC,
+      TALB: tags.TALB,
+      TPE1: tags.TPE1,
+      TIT2: tags.TIT2,
+      TXXX: tags.TXXX,
+      COMM: tags.COMM
+    }
+    callback(null, _extra);
+  })
+}
 
 /**
  * @method removeByUri
