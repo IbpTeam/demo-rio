500c82a7018bf61191aab47bd4744284e3f34134
rtty122333 <wangbo7799@gmail.com>
2015-04-28 10:58:44 +0800

    change require path of requireProxy

--- sdk/lib/requireProxy.js
+++ sdk/lib/requireProxy.js
@@ -1,7 +1,14 @@
 var util = require('util');
 var net = require('net');
+var fs = require('fs');
 // RPC API lib.
 console.log("head of requireProxy.js.");
+
+var debug = true;
+
+var LOCALPATH = "/usr/local/share/webde/proxy/";
+var USERPATH = process.env.HOME + "/.local/share/webde/proxy/";
+
 /* requireProxy takes 2 or 3 parameters, if you want to call a local service ,
     take 2 parameters as follows:
  requireProxy(['mix',function(mix){
@@ -17,17 +24,27 @@ requireProxy(['mix'],"127.0.0.1",function(mix){
 });
 */
 exports.requireProxy = function() {
-  if (typeof (arguments[arguments.length-1]) !== "function" ) {
+  if (typeof(arguments[arguments.length - 1]) !== "function") {
     console.log("the last argument should be function");
     return false;
   };
   if (arguments.length === 2) {
     util.log("requireLocalProxy:" + arguments[0]);
     var i;
-    var callback = arguments[arguments.length-1];
+    var callback = arguments[arguments.length - 1];
     var apiArr = new Array(arguments[0].length);
-    for (i = 0; i < arguments[0].length; i += 1) {
-      apiArr[i] = require('../../service/' + arguments[0][i] + '/interface/' + arguments[0][i] + 'Proxy').getProxy();
+    if (debug === true) {
+      for (i = 0; i < arguments[0].length; i += 1) {
+        apiArr[i] = require('../../service/' + arguments[0][i] + '/interface/' + arguments[0][i] + 'Proxy').getProxy();
+      }
+    } else {
+      if (fs.existsSync(USERPATH + '/' + arguments[0][i] + '/' + arguments[0][i] + 'proxy.js')) {
+        apiArr[i] = require(USERPATH + '/' + arguments[0][i] + '/' + arguments[0][i] + 'proxy').getProxy();
+      } else if (fs.existsSync(LOCALPATH + '/' + arguments[0][i] + '/' + arguments[0][i] + 'proxy.js')) {
+        apiArr[i] = require(LOCALPATH + '/' + arguments[0][i] + '/' + arguments[0][i] + 'proxy').getProxy();
+      } else {
+        console.log("Error : service proxy not found");
+      }
     }
     setTimeout(function() {
       callback.apply(null, apiArr)
@@ -40,14 +57,23 @@ exports.requireProxy = function() {
       return false;
     };
     var i;
-    var callback = arguments[arguments.length-1];
+    var callback = arguments[arguments.length - 1];
     var apiArr = new Array(arguments[0].length);
-    for (i = 0; i < arguments[0].length; i += 1) {
-      apiArr[i] = require('../../service/' + arguments[0][i] + '/interface/' + arguments[0][i] + 'ProxyRemote').getProxy(arguments[1]);
+    if (debug === true) {
+      for (i = 0; i < arguments[0].length; i += 1) {
+        apiArr[i] = require('../../service/' + arguments[0][i] + '/interface/' + arguments[0][i] + 'ProxyRemote').getProxy(arguments[1]);
+      }
+    } else {
+      if (fs.existsSync(USERPATH + '/' + arguments[0][i] + '/' + arguments[0][i] + 'ProxyRemote.js')) {
+        apiArr[i] = require(USERPATH + '/' + arguments[0][i] + '/' + arguments[0][i] + 'ProxyRemote').getProxy(arguments[1]);
+      } else if (fs.existsSync(LOCALPATH + '/' + arguments[0][i] + '/' + arguments[0][i] + 'ProxyRemote.js')) {
+        apiArr[i] = require(LOCALPATH + '/' + arguments[0][i] + '/' + arguments[0][i] + 'ProxyRemote').getProxy(arguments[1]);
+      } else {
+        console.log("Error : service proxy not found");
+      }
     }
     setTimeout(function() {
       callback.apply(null, apiArr)
     }, 0);
   };
-
 }
