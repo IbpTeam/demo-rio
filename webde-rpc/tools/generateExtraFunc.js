e6372d187d6de89f63a53364e906a350fc2ee63f
rtty122333 <wangbo7799@gmail.com>
2015-05-06 15:06:08 +0800

    a tool for generate Extra funcs

--- webde-rpc/tools/generateExtraFunc.js
+++ webde-rpc/tools/generateExtraFunc.js
@@ -1,16 +1,32 @@
-// generate Proxy and Stub automatically base on user defined interface
-//
 if (process.argv.length < 3)
-  return console.log('Usage: node generator.js ${Your_Interface_File} [ipc type]');
+  return console.log('Usage: node generateExtraFunc.js ${old_Interface_File} ${new_Interface_File}');
 
 var fs = require('fs'),
   util = require('util'),
   events = require('events'),
-  json4line = require('../../sdk/utils/json4line');
+  json4line = require('../../sdk/utils/json4line'),
+  hashtable = require('hashtable');
 
-json4line.readJSONFile(process.argv[2], function(err, interfaces) {
+json4line.readJSONFile(process.argv[2], function(err, file1) {
+  var table = new hashtable();
   if (err) return console.log('Interface File error:', err);
-  builder(interfaces);
+  json4line.readJSONFile(process.argv[3], function(err, file2) {
+    if (err) return console.log('Interface File error:', err);
+    for (var i = 1; i < file1.interfaces.length; i++) {
+      table.put(file1.interfaces[i].name, file1.interfaces[i]);
+    }
+    var ifa = new Object();
+    var iter = [];
+    for (var i = 1; i < file2.interfaces.length; i++) {
+      if (table.get(file2.interfaces[i].name) === undefined) {
+        iter.push(file2.interfaces[i]);
+      }
+    }
+    ifa.service = file2.service;
+    ifa.remote = file2.remote;
+    ifa.interfaces = iter;
+    builder(ifa);
+  });
 });
 
 function builder(ifaces) {
