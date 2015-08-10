2b89a73787be80322f72160c2d295ee3fc7235ba
xwh <weihong@iscas.ac.cn>
2015-08-10 10:57:02 +0800

    WEB-8:js

--- newdatamgr/js/usrInfo.js
+++ newdatamgr/js/usrInfo.js
@@ -1,16 +1,16 @@
 var UsrInfoView = Class.extend({
-  init:function(){
-  	this._usrInfoContainer = $('<div>',{
-  	  'id' : 'usrInfo-container'
-  	});
-    this._closeDiv = $('<div>',{
-      'id' : 'closeDiv'
+  init: function() {
+    this._usrInfoContainer = $('<div>', {
+      'id': 'usrInfo-container'
     });
-    this._usrInfoDiv = $('<div>',{
-      'id' : 'usrInfoDiv'
+    this._closeDiv = $('<div>', {
+      'id': 'closeDiv'
     });
-    this._usrExtraDiv = $('<div>',{
-      'id' : 'usrExtraDiv'
+    this._usrInfoDiv = $('<div>', {
+      'id': 'usrInfoDiv'
+    });
+    this._usrExtraDiv = $('<div>', {
+      'id': 'usrExtraDiv'
     });
     this._defaultHeadPhoto = 'img/localhost.jpg';
     this._usrInfoContainer.append(this._closeDiv);
@@ -21,49 +21,68 @@ var UsrInfoView = Class.extend({
     //this.setUsrInfo();
     //this.setUsrExtra('load');
   },
-  setUsrInfo:function(){
+  setUsrInfo: function() {
     var _headPhotoSrc = '';
     var _tempName = 'Rio';
     var _tmpIp = '192.168.162.7';
     var _tmpDevices = 'deviceId';
     var _tmpOthers = 'this is others';
-    if(true)
+    if (true)
       _headPhotoSrc = this._defaultHeadPhoto;
-    var _usrHeadShow = $('<div>',{
-      'id' : 'usrHeadShow'
+    var _usrHeadShow = $('<div>', {
+      'id': 'usrHeadShow'
     });
-    var _usrInfoShow = $('<div>',{
-      'id' : 'usrInfoShow'
+    var _usrInfoShow = $('<div>', {
+      'id': 'usrInfoShow'
     });
-    var _usrPhotoDiv = $('<div>',{
-      'id' : 'usrPhotoDiv'
+    var _toggleTrigglerShow = $('<div>', {
+      'id': 'toggleTrigglerShow'
     });
-    var _usrEditDiv = $('<div>',{
-      'id' : 'usrEditDiv',
-      'text' : 'Change'
+    var _usrPhotoDiv = $('<div>', {
+      'id': 'usrPhotoDiv'
     });
-    var _headPhotoImg = $('<img>',{
-      'id' : 'headPhotoImg',
-      'src' : _headPhotoSrc
+    var _usrEditDiv = $('<div>', {
+      'id': 'usrEditDiv',
+      'text': 'Change'
     });
-    var _usrInfoUl = $('<ul>',{
-      'id' : 'usrInfoUl'
+    var _headPhotoImg = $('<img>', {
+      'id': 'headPhotoImg',
+      'src': _headPhotoSrc
     });
-    var _usrName = $('<li>',{
-      'class' : 'usrInfo-ul',
-      'text' : 'Name: ' + _tempName
+    var _usrInfoUl = $('<ul>', {
+      'id': 'usrInfoUl'
     });
-    var _usrIp = $('<li>',{
-      'class' : 'usrInfo-ul',
-      'text' : 'IP: ' + _tmpIp
+    var _usrName = $('<li>', {
+      'class': 'usrInfo-ul',
+      'text': 'Name: ' + _tempName
     });
-    var _usrDevices = $('<li>',{
-      'class' : 'usrInfo-ul',
-      'text' : 'Devices: ' + _tmpDevices
+    var _usrIp = $('<li>', {
+      'class': 'usrInfo-ul',
+      'text': 'IP: ' + _tmpIp
     });
-    var _usrOthers = $('<li>',{
-      'class' : 'usrInfo-ul',
-      'text' : 'Others: ' + _tmpOthers
+    var _usrDevices = $('<li>', {
+      'class': 'usrInfo-ul',
+      'text': 'Devices: ' + _tmpDevices
+    });
+    var _usrOthers = $('<li>', {
+      'class': 'usrInfo-ul',
+      'text': 'Others: ' + _tmpOthers
+    });
+    var _toggleTrigglerButton = $('<span>', {
+      'id': 'toggleTrigglerButton',
+      'text': 'Package'
+    });
+    _toggleTrigglerButton.on('click',function() {
+      if($('buttonOne').text === "Data"){
+        doucument.getElementById('buttonOne').innerText = "Import";
+        doucument.getElementById('buttonTwo').innerText = "Export";
+        doucument.getElementById('uploadName').innerText = "Confirm";
+      }else{
+        doucument.getElementById('buttonOne').innerText = "Contacts";
+        doucument.getElementById('buttonTwo').innerText = "Data";
+        doucument.getElementById('uploadName').innerText = "Import";
+      }
+
     });
     _usrInfoUl.append(_usrName);
     _usrInfoUl.append(_usrIp);
@@ -73,159 +92,187 @@ var UsrInfoView = Class.extend({
     _usrPhotoDiv.append(_headPhotoImg);
     _usrHeadShow.append(_usrPhotoDiv);
     _usrHeadShow.append(_usrEditDiv);
+    _toggleTrigglerShow.append(_toggleTrigglerButton);
     this._usrInfoDiv.append(_usrHeadShow);
     this._usrInfoDiv.append(_usrInfoShow);
+    this._usrInfoDiv.append(_toggleTrigglerShow);
 
   },
-  setUsrExtra:function(extraAction_){
-    switch(extraAction_){
-      case 'load' : {
-        this.setExtraLoad();
-      }
-      break;
-      default : {
+  setUsrExtra: function(extraAction_) {
+    switch (extraAction_) {
+      case 'load':
+        {
+          this.setExtraLoad();
+        }
+        break;
+      default:
+        {
 
-      }
+        }
     }
   },
-  setExtraLoad:function(){
+  setExtraLoad: function() {
     var _this = this;
-    var _extraLoadDiv = $('<div>',{
-      'id' : 'extraLoadDiv'
+    var _extraLoadDiv = $('<div>', {
+      'id': 'extraLoadDiv'
     });
 
-    var _loadContactBg = $('<div>',{
-      'id' : 'loadContactBg'
+    var _loadContactBg = $('<div>', {
+      'id': 'loadContactBg'
     });
-    var _loadContactDiv = $('<div>',{
-      'id' : 'loadContactDiv'
+    var _loadContactDiv = $('<div>', {
+      'id': 'loadContactDiv'
     });
-    var _contactLabel = $('<label>',{
+    var _contactLabel = $('<label>', {
       //'id' : 'contactLabel',
       //'text' : 'Contacts',
       'for': 'contactCheckBox'
     });
-    var _contactCheckBox = $('<input>',{
-      'id' : 'contactCheckBox',
-      'type' : 'checkbox',
-      'checked' : 'checked',
-      'click' : function(){
-      $('#dataCheckBox')[0].checked = false;
-      $('#fileUpLoad')[0].nwdirectory = false;
+    var _contactCheckBox = $('<input>', {
+      'id': 'contactCheckBox',
+      'type': 'checkbox',
+      'checked': 'checked',
+      'click': function() {
+        $('#dataCheckBox')[0].checked = false;
+        $('#fileUpLoad')[0].nwdirectory = false;
       }
     });
     _loadContactDiv.append(_contactCheckBox);
     _loadContactDiv.append(_contactLabel);
-    _loadContactDiv.append('<label class="contactSpan" for="contactCheckBox">Contacts</label>');
+    _loadContactDiv.append('<label id="buttonOne" class="contactSpan" for="contactCheckBox">Contacts</label>');
     _loadContactBg.append(_loadContactDiv);
 
-    var _loadDataBg = $('<div>',{
-      'id' : 'loadDataBg'
+    var _loadDataBg = $('<div>', {
+      'id': 'loadDataBg'
     });
-    var _loadDataDiv = $('<div>',{
-      'id' : 'loadDataDiv'
+    var _loadDataDiv = $('<div>', {
+      'id': 'loadDataDiv'
     });
-    var _dataLabel = $('<label>',{
+    var _dataLabel = $('<label>', {
       //'id' : 'dataLabel',
       //'text' : 'Data'
       'for': 'dataCheckBox'
     });
-    var _dataCheckBox = $('<input>',{
-      'id' : 'dataCheckBox',
-      'type' : 'checkbox',
-      'click' : function(){
-      $('#contactCheckBox')[0].checked = false;
-      //var _tmpObj = $('#fileUpLoad');
-      //alert(111);
-      $('#fileUpLoad')[0].nwdirectory = true;
+    var _dataCheckBox = $('<input>', {
+      'id': 'dataCheckBox',
+      'type': 'checkbox',
+      'click': function() {
+        $('#contactCheckBox')[0].checked = false;
+        //var _tmpObj = $('#fileUpLoad');
+        //alert(111);
+        $('#fileUpLoad')[0].nwdirectory = true;
       }
     });
     _loadDataDiv.append(_dataCheckBox);
     _loadDataDiv.append(_dataLabel);
-    _loadDataDiv.append('<label class="dataSpan" for="dataCheckBox">Datas</label>');
+    _loadDataDiv.append('<label id="buttonTwo" class="dataSpan" for="dataCheckBox">Data</label>');
     _loadDataBg.append(_loadDataDiv);
 
-    var _uploadDiv = $('<div>',{
-      'id' : 'uploadDiv'
+    var _uploadDiv = $('<div>', {
+      'id': 'uploadDiv'
     });
-    var _upLoadName = $('<span>',{
-      'id' : 'uploadName',
+    var _upLoadName = $('<span>', {
+      'id': 'uploadName',
       'text': 'Import'
     });
-    var _fileUpLoad = $('<input>',{
-      'id' : 'fileUpLoad',
-      'type' : 'file'
+    var _fileUpLoad = $('<input>', {
+      'id': 'fileUpLoad',
+      'type': 'file'
     });
-    _fileUpLoad.on('change',function(){
+    _fileUpLoad.on('change', function() {
         var _modalBoxObj = _this._modalBox;
         _modalBoxObj.forbidClose(true);
         document.body.style.cursor = "wait";
         var resourcePath = this.value;
         if ($('#contactCheckBox')[0].checked) {
-          DataAPI.loadContacts(function(err,result){
-            document.body.style.cursor = "default";
-            _modalBoxObj.forbidClose(false);
-            _this._isLoadedResources = true;
-          },resourcePath);
-        }else if ($('#dataCheckBox')[0].checked) {
-          DataAPI.loadResources(function(err, result){
-            if(err){
+          if (doucument.getElementById('buttonOne').innerText === 'Contacts') {
+            DataAPI.loadContacts(function(err, result) {
+              document.body.style.cursor = "default";
+              _modalBoxObj.forbidClose(false);
+              _this._isLoadedResources = true;
+            }, resourcePath);
+          } else if (doucument.getElementById('buttonOne').innerText=== 'Import') {
+            DataAPI.importData(function (err, result) {
+              document.body.style.cursor = "default";
+              _modalBoxObj.forbidClose(false);
+              _this._isLoadedResources = true;
+            }, "~/.custardBac")
+          } else {
+          //ToDo-err handle
+        }
+      } else if ($('#dataCheckBox')[0].checked) {
+        if (doucument.getElementById('buttonTwo').innerText === 'Data') {
+          DataAPI.loadResources(function(err, result) {
+            if (err) {
               throw err;
             }
             document.body.style.cursor = "default";
             _modalBoxObj.forbidClose(false);
             _this._isLoadedResources = true;
-          },resourcePath);
-        }else{
+          }, resourcePath);
+        } else if (doucument.getElementById('buttonTwo').innerText === 'Export') {
+          DataAPI.exportData(function(err, result) {
+              if (err) {
+                throw err;
+              }
+              document.body.style.cursor = "default";
+              _modalBoxObj.forbidClose(false);
+              _this._isLoadedResources = true;
+            }, "~/.custardBac");
+
+        } else {
           //ToDo-err handle
         }
+      } else {
+        //ToDo-err handle
+      }
     });
-    _uploadDiv.append(_fileUpLoad);
-    _uploadDiv.append(_upLoadName);
-    _extraLoadDiv.append(_loadContactBg);
-    _extraLoadDiv.append(_loadDataBg);
-    _extraLoadDiv.append(_uploadDiv);
-    this._usrExtraDiv.append(_extraLoadDiv);
-  },
-  showUsrInfo:function(){
-    var _this = this;
-    this._modalBox = ModalBox.create($('#usrInfo-container'),{
-      iconClose: false,                
-      keyClose:true,                      
-      bodyClose:true,
-      onClose: function(){
-        if(_this._isLoadedResources){
-          var _window = undefined;
-          if (window == top){
-            parent.location.reload();
-          }else{
-            try{
-              _window = parent._global._openingWindows.getCOMById('datamgr-app-window');
-              var _dataMgrIfm = _window._windowContent[0];
-              _dataMgrIfm.src = _dataMgrIfm.src;
-            } catch(e){
-              console.log(e);
-            }
+  _uploadDiv.append(_fileUpLoad);
+  _uploadDiv.append(_upLoadName);
+  _extraLoadDiv.append(_loadContactBg);
+  _extraLoadDiv.append(_loadDataBg);
+  _extraLoadDiv.append(_uploadDiv);
+  this._usrExtraDiv.append(_extraLoadDiv);
+},
+showUsrInfo: function() {
+  var _this = this;
+  this._modalBox = ModalBox.create($('#usrInfo-container'), {
+    iconClose: false,
+    keyClose: true,
+    bodyClose: true,
+    onClose: function() {
+      if (_this._isLoadedResources) {
+        var _window = undefined;
+        if (window == top) {
+          parent.location.reload();
+        } else {
+          try {
+            _window = parent._global._openingWindows.getCOMById('datamgr-app-window');
+            var _dataMgrIfm = _window._windowContent[0];
+            _dataMgrIfm.src = _dataMgrIfm.src;
+          } catch (e) {
+            console.log(e);
           }
-          _this._isLoadedResources = false;
         }
-      }                    
-    });
-    var _modalBoxObj = this._modalBox;
-    this._closeDiv.on('click',function(){
-      _modalBoxObj.close();
-    });
-  },
-  closeUsrInfo:function(){
-    this._modalBox.close();
-  },
-  attach:function($parent_){
-    $parent_.append(this._usrInfoContainer);
-  },
-  hide:function(){
-    this._usrInfoContainer.hide();
-  },
-  show:function(){
-    this._usrInfoContainer.show();
-  }
+        _this._isLoadedResources = false;
+      }
+    }
+  });
+  var _modalBoxObj = this._modalBox;
+  this._closeDiv.on('click', function() {
+    _modalBoxObj.close();
+  });
+},
+closeUsrInfo: function() {
+  this._modalBox.close();
+},
+attach: function($parent_) {
+  $parent_.append(this._usrInfoContainer);
+},
+hide: function() {
+  this._usrInfoContainer.hide();
+},
+show: function() {
+  this._usrInfoContainer.show();
+}
 });
